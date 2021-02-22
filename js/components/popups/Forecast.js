e6b950229583df643fcef057a54f02c98da8100d
Yevhenii Molodyi <eugmolody@gmail.com>
2021-02-22 17:49:56 +0200

    Replace Forecast by react hooks implementation

--- js/components/popups/Forecast.js
+++ js/components/popups/Forecast.js
@@ -7,9 +7,11 @@ exports["default"] = void 0;
 
 var _extends2 = _interopRequireDefault(require("@babel/runtime/helpers/extends"));
 
-var _inheritsLoose2 = _interopRequireDefault(require("@babel/runtime/helpers/inheritsLoose"));
-
 var _react = _interopRequireDefault(require("../_react"));
+
+var _reactRedux = require("react-redux");
+
+var _useTheme = _interopRequireDefault(require("../hooks/useTheme"));
 
 var _selectors = require("../../flux/selectors");
 
@@ -19,94 +21,47 @@ var _PeriodForecast = _interopRequireDefault(require("../views/PeriodForecast"))
 
 var _Forecast = _interopRequireDefault(require("./Forecast.Style"));
 
-var _withTheme = _interopRequireDefault(require("../hoc/withTheme"));
-
-//import React, { Component } from 'react';
 //import PropTypes from 'prop-types';
-var Component = _react["default"].Component;
 var S = {
   CAPTION: {
-    marginRight: '40px'
+    marginRight: 40
   }
 };
 var NOT_FOUND_MSG = 'Forecast for place not found';
 var OK_CODE = '200';
-var INIT_STATE = {
-  forecast: {
-    cod: 200,
-    list: []
-  }
+
+var NotFoundMsg = function NotFoundMsg() {
+  return /*#__PURE__*/_react["default"].createElement("div", null, /*#__PURE__*/_react["default"].createElement("span", null, NOT_FOUND_MSG));
 };
 
-var Forecast =
-/*#__PURE__*/
-function (_Component) {
-  (0, _inheritsLoose2["default"])(Forecast, _Component);
+var Forecast = function Forecast(_ref) {
+  var style = _ref.style;
 
-  function Forecast() {
-    var _this;
+  var forecast = (0, _reactRedux.useSelector)(function (state) {
+    var recent = _selectors.sForecast.recent(state);
 
-    for (var _len = arguments.length, args = new Array(_len), _key = 0; _key < _len; _key++) {
-      args[_key] = arguments[_key];
-    }
+    return _selectors.sForecast.byId(state, recent);
+  }),
+      _style = (0, _useTheme["default"])(_Forecast["default"]);
 
-    _this = _Component.call.apply(_Component, [this].concat(args)) || this;
-    _this.state = INIT_STATE;
+  var _ref2 = forecast || {},
+      cod = _ref2.cod;
 
-    _this._onStore = function () {
-      var store = _this.props.store,
-          state = store.getState(),
-          recent = _selectors.sForecast.recent(state);
+  return /*#__PURE__*/_react["default"].createElement(_FlyPopup["default"], {
+    style: (0, _extends2["default"])({}, style, _style.ROOT_DIV),
+    storeKey: "isPopupForecast"
+  }, /*#__PURE__*/_react["default"].createElement(_PeriodForecast["default"], {
+    forecast: forecast,
+    captionStyle: S.CAPTION
+  }), cod && '' + cod !== OK_CODE && /*#__PURE__*/_react["default"].createElement(NotFoundMsg, null));
+};
+/*
+Forecast.propTypes = {
+  style: PropTypes.object
+}
+*/
 
-      if (recent && _this.recent !== recent) {
-        _this.setState({
-          forecast: _selectors.sForecast.byId(state, recent)
-        });
-      }
-    };
 
-    _this._renderMsg = function () {
-      return _react["default"].createElement("div", null, _react["default"].createElement("span", null, NOT_FOUND_MSG));
-    };
-
-    return _this;
-  }
-
-  var _proto = Forecast.prototype;
-
-  _proto.componentDidMount = function componentDidMount() {
-    var store = this.props.store;
-    this.unsubscribe = store.subscribe(this._onStore);
-  };
-
-  _proto.componentWillUnmount = function componentWillUnmount() {
-    this.unsubscribe();
-  };
-
-  _proto.render = function render() {
-    var _this$props = this.props,
-        rootStyle = _this$props.rootStyle,
-        store = _this$props.store,
-        theme = _this$props.theme,
-        forecast = this.state.forecast,
-        cod = forecast.cod,
-        _style = theme.createStyle(_Forecast["default"]);
-
-    return _react["default"].createElement(_FlyPopup["default"], {
-      rootStyle: (0, _extends2["default"])({}, rootStyle, {}, _style.ROOT_DIV),
-      store: store,
-      storeKey: "isPopupForecast",
-      isShow: true
-    }, _react["default"].createElement(_PeriodForecast["default"], {
-      forecast: forecast,
-      captionStyle: S.CAPTION
-    }), '' + cod !== OK_CODE && this._renderMsg());
-  };
-
-  return Forecast;
-}(Component);
-
-var _default = (0, _withTheme["default"])(Forecast);
-
+var _default = Forecast;
 exports["default"] = _default;
 //# sourceMappingURL=Forecast.js.map
--- src/components/popups/Forecast.jsx
+++ src/components/popups/Forecast.jsx
@@ -1,91 +1,57 @@
-//import React, { Component } from 'react';
 import React from '../_react'
 //import PropTypes from 'prop-types';
+import { useSelector } from 'react-redux'
+import useTheme from '../hooks/useTheme'
 
 import { sForecast } from '../../flux/selectors';
 
 import FlyPopup from '../containers/FlyPopup';
 import PeriodForecast from '../views/PeriodForecast';
-
 import styleConfig from './Forecast.Style';
-import withTheme from '../hoc/withTheme';
-
-const { Component } = React
 
 const S  = {
   CAPTION: {
-    marginRight: '40px'
+    marginRight: 40
   }
 };
 
 const NOT_FOUND_MSG = 'Forecast for place not found';
 const OK_CODE = '200'
 
-const INIT_STATE = {
-  forecast : {
-    cod : 200,
-    list : []
-  }
+const NotFoundMsg = () => (
+  <div>
+    <span>{NOT_FOUND_MSG}</span>
+  </div>
+);
+
+
+const Forecast = ({ style }) => {
+  const forecast = useSelector(state => {
+    const recent = sForecast.recent(state)
+    return sForecast.byId(state, recent);
+  })
+  , _style = useTheme(styleConfig);
+
+  const { cod } = forecast || {};
+
+  return (
+    <FlyPopup
+        style={{...style, ..._style.ROOT_DIV}}
+        storeKey="isPopupForecast"
+     >
+      <PeriodForecast
+          forecast={forecast}
+          captionStyle={S.CAPTION}
+      />
+      { cod && (''+cod) !== OK_CODE && <NotFoundMsg /> }
+    </FlyPopup>
+  );
 }
 
-class Forecast extends Component {
-  /*
-  static propTypes = {
-    store: PropTypes.object
-  }
-  */
-
-  state = INIT_STATE
-
-  componentDidMount(){
-     const { store } = this.props
-     this.unsubscribe = store.subscribe(this._onStore)
-  }
-
-  _onStore = () => {
-    const { store } = this.props
-    , state = store.getState()
-    , recent = sForecast.recent(state)
-    if (recent && this.recent !== recent ){
-      this.setState({
-        forecast : sForecast.byId(state, recent)
-      })
-    }
-  }
-
-  componentWillUnmount(){
-    this.unsubscribe();
-  }
-
-  _renderMsg = () => {
-    return (
-      <div>
-        <span>{NOT_FOUND_MSG}</span>
-      </div>
-    )
-  }
-
-  render(){
-    const { rootStyle, store, theme } = this.props
-        , { forecast } = this.state
-        , { cod } = forecast
-        , _style = theme.createStyle(styleConfig);
-
-    return (
-      <FlyPopup
-          rootStyle={{ ...rootStyle, ..._style.ROOT_DIV }}
-          store={store}
-          storeKey="isPopupForecast"
-          isShow={true}
-       >
-        <PeriodForecast
-            forecast={forecast}
-            captionStyle={S.CAPTION}
-        />
-        { (''+cod) !== OK_CODE && this._renderMsg() }
-      </FlyPopup>
-    );
-  }
+/*
+Forecast.propTypes = {
+  style: PropTypes.object
 }
+*/
 
-export default withTheme(Forecast)
+export default Forecast
