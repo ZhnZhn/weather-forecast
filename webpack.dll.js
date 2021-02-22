f4aec47aab868593c474ffaadfdc85bd68e05347
Yevhenii Molodyi <eugmolody@gmail.com>
2021-02-22 17:42:08 +0200

    Add react-redux

--- webpack.dll.js
+++ webpack.dll.js
@@ -9,7 +9,7 @@ module.exports = {
   mode: "production",
   entry: {
      lib: [
-            "redux", "redux-saga",
+            "redux", "react-redux", "redux-saga",
             "interactjs"
           ]
   },
