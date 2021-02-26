e03f4645898a034aae430ec027c28b397e4883cb
Yevhenii Molodyi <eugmolody@gmail.com>
2021-02-26 14:49:39 +0200

    Add SpinnerLoading

---
+++ js/components/zhn-atoms/SpinnerLoading.js
@@ -0,0 +1,20 @@
+"use strict";
+
+var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
+
+exports.__esModule = true;
+exports["default"] = void 0;
+
+var _react = _interopRequireDefault(require("../_react"));
+
+var SpinnerLoading = function SpinnerLoading(_ref) {
+  var style = _ref.style;
+  return /*#__PURE__*/_react["default"].createElement("div", {
+    "data-loader": "circle",
+    style: style
+  });
+};
+
+var _default = SpinnerLoading;
+exports["default"] = _default;
+//# sourceMappingURL=SpinnerLoading.js.map
---
+++ src/components/zhn-atoms/SpinnerLoading.jsx
@@ -0,0 +1,10 @@
+import React from '../_react'
+
+const SpinnerLoading = ({ style }) => (
+  <div
+    data-loader="circle"
+    style={style}
+  />
+);
+
+export default SpinnerLoading
