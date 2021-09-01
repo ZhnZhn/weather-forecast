e74023449b2dce2ea0fde02f39a10321ffe9c962
Yevhenii Molodyi <eugmolody@gmail.com>
2021-09-01 10:38:35 +0300

    Update recharts to 2.1.2

--- js/components/charts/Chart.js
+++ js/components/charts/Chart.js
@@ -1,41 +1,39 @@
 "use strict";
 
-var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
-
 exports.__esModule = true;
 exports["default"] = void 0;
 
-var _CartesianGrid = _interopRequireDefault(require("recharts/es6/cartesian/CartesianGrid"));
+var _CartesianGrid = require("recharts/es6/cartesian/CartesianGrid");
 
-var _Bar = _interopRequireDefault(require("recharts/es6/cartesian/Bar"));
+var _Bar = require("recharts/es6/cartesian/Bar");
 
-var _Line = _interopRequireDefault(require("recharts/es6/cartesian/Line"));
+var _Line = require("recharts/es6/cartesian/Line");
 
-var _YAxis = _interopRequireDefault(require("recharts/es6/cartesian/YAxis"));
+var _YAxis = require("recharts/es6/cartesian/YAxis");
 
-var _XAxis = _interopRequireDefault(require("recharts/es6/cartesian/XAxis"));
+var _XAxis = require("recharts/es6/cartesian/XAxis");
 
-var _ResponsiveContainer = _interopRequireDefault(require("recharts/es6/component/ResponsiveContainer"));
+var _ResponsiveContainer = require("recharts/es6/component/ResponsiveContainer");
 
-var _Tooltip = _interopRequireDefault(require("recharts/es6/component/Tooltip"));
+var _Tooltip = require("recharts/es6/component/Tooltip");
 
-var _Legend = _interopRequireDefault(require("recharts/es6/component/Legend"));
+var _Legend = require("recharts/es6/component/Legend");
 
-var _ComposedChart = _interopRequireDefault(require("recharts/es6/chart/ComposedChart"));
+var _ComposedChart = require("recharts/es6/chart/ComposedChart");
 
-var _LineChart = _interopRequireDefault(require("recharts/es6/chart/LineChart"));
+var _LineChart = require("recharts/es6/chart/LineChart");
 
 var Chart = {
-  CartesianGrid: _CartesianGrid["default"],
-  Bar: _Bar["default"],
-  Line: _Line["default"],
-  YAxis: _YAxis["default"],
-  XAxis: _XAxis["default"],
-  ResponsiveContainer: _ResponsiveContainer["default"],
-  Tooltip: _Tooltip["default"],
-  Legend: _Legend["default"],
-  ComposedChart: _ComposedChart["default"],
-  LineChart: _LineChart["default"]
+  CartesianGrid: _CartesianGrid.CartesianGrid,
+  Bar: _Bar.Bar,
+  Line: _Line.Line,
+  YAxis: _YAxis.YAxis,
+  XAxis: _XAxis.XAxis,
+  ResponsiveContainer: _ResponsiveContainer.ResponsiveContainer,
+  Tooltip: _Tooltip.Tooltip,
+  Legend: _Legend.Legend,
+  ComposedChart: _ComposedChart.ComposedChart,
+  LineChart: _LineChart.LineChart
 };
 var _default = Chart;
 exports["default"] = _default;
--- src/components/charts/Chart.js
+++ src/components/charts/Chart.js
@@ -1,16 +1,16 @@
 
-import CartesianGrid from "recharts/es6/cartesian/CartesianGrid"
-import Bar from "recharts/es6/cartesian/Bar"
-import Line from "recharts/es6/cartesian/Line"
-import YAxis from "recharts/es6/cartesian/YAxis"
-import XAxis from "recharts/es6/cartesian/XAxis"
+import { CartesianGrid } from "recharts/es6/cartesian/CartesianGrid"
+import { Bar } from "recharts/es6/cartesian/Bar"
+import { Line } from "recharts/es6/cartesian/Line"
+import { YAxis } from "recharts/es6/cartesian/YAxis"
+import { XAxis } from "recharts/es6/cartesian/XAxis"
 
-import ResponsiveContainer from "recharts/es6/component/ResponsiveContainer"
-import Tooltip from "recharts/es6/component/Tooltip"
-import Legend from "recharts/es6/component/Legend"
+import { ResponsiveContainer } from "recharts/es6/component/ResponsiveContainer"
+import { Tooltip } from "recharts/es6/component/Tooltip"
+import { Legend } from "recharts/es6/component/Legend"
 
-import ComposedChart from "recharts/es6/chart/ComposedChart";
-import LineChart from "recharts/es6/chart/LineChart";
+import { ComposedChart } from "recharts/es6/chart/ComposedChart";
+import { LineChart } from "recharts/es6/chart/LineChart";
 
 const Chart = {
   CartesianGrid,
