9dfff6afcf37559bdc799299120d34e83a7364d8
Yevhenii Molodyi <eugmolody@gmail.com>
2021-12-21 14:38:01 +0200

    Edit DayDetailPopup style

--- js/components/left-push-menu/DayDetailPopup.js
+++ js/components/left-push-menu/DayDetailPopup.js
@@ -18,42 +18,38 @@ var _theme = require("../styles/theme");
 var forwardRef = _react["default"].forwardRef,
     useState = _react["default"].useState,
     useImperativeHandle = _react["default"].useImperativeHandle;
-var CL = {
-  DATE: 'marker__caption__date',
-  DESCR: 'marker__description',
-  LABEL: 'marker__label',
-  V_RAIN: 'marker__v-rain',
-  V_WATER: 'marker__v-water',
-  V_PRESSURE: 'marker__v-pressure',
-  V_DAY: 'marker__v-day',
-  V_NIGHT: 'marker__v-night'
-};
-var STYLE = {
-  ROOT_DIV: {
-    position: 'absolute',
-    top: 190,
-    left: 200,
-    padding: '8px 8px',
-    lineHeight: 1.5,
-    borderRadius: 4,
-    boxShadow: 'rgba(0, 0, 0, 0.2) 0px 0px 0px 6px',
-    zIndex: 1,
-    transition: 'left 0.5s ease-in 0s'
-  },
-  BLOCK: {
-    display: 'block'
-  },
-  NONE: {
-    display: 'none'
-  },
-  BT_CLOSE: {
-    position: 'absolute',
-    top: 7,
-    right: 4
-  },
-  DAY: {
-    borderBottom: '2px solid #8bc34a'
-  }
+var CL_DATE = 'marker__caption__date',
+    CL_DESCR = 'marker__description',
+    CL_LABEL = 'marker__label',
+    CL_V_RAIN = 'marker__v-rain',
+    CL_V_WATER = 'marker__v-water',
+    CL_V_PRESSURE = 'marker__v-pressure',
+    CL_V_DAY = 'marker__v-day',
+    CL_V_NIGHT = 'marker__v-night',
+    S_ROOT_DIV = {
+  position: 'absolute',
+  top: 190,
+  left: 200,
+  padding: '8px 8px',
+  lineHeight: 1.5,
+  borderRadius: 4,
+  boxShadow: 'rgba(0, 0, 0, 0.2) 0px 0px 0px 6px',
+  zIndex: 1,
+  transition: 'left 0.5s ease-in 0s'
+},
+    S_BLOCK = {
+  display: 'block'
+},
+    S_NONE = {
+  display: 'none'
+},
+    S_BT_CLOSE = {
+  position: 'absolute',
+  top: 7,
+  right: 4
+},
+    S_DAY = {
+  borderBottom: '2px solid #8bc34a'
 };
 
 var TitleValue = function TitleValue(_ref) {
@@ -61,7 +57,7 @@ var TitleValue = function TitleValue(_ref) {
       valueCn = _ref.valueCn,
       value = _ref.value;
   return /*#__PURE__*/_react["default"].createElement(_react["default"].Fragment, null, /*#__PURE__*/_react["default"].createElement("span", {
-    className: CL.LABEL
+    className: CL_LABEL
   }, title, "\xA0"), /*#__PURE__*/_react["default"].createElement("span", {
     className: valueCn
   }, value, "\xA0"));
@@ -97,10 +93,8 @@ var DayDetailPopup = forwardRef(function (_ref2, ref) {
 
   var _ref3 = item || {},
       timestamp = _ref3.dt,
-      _ref3$rain = _ref3.rain,
-      rain = _ref3$rain === void 0 ? 0 : _ref3$rain,
-      _ref3$snow = _ref3.snow,
-      snow = _ref3$snow === void 0 ? 0 : _ref3$snow,
+      rain = _ref3.rain,
+      snow = _ref3.snow,
       _ref3$clouds = _ref3.clouds,
       clouds = _ref3$clouds === void 0 ? 0 : _ref3$clouds,
       _ref3$humidity = _ref3.humidity,
@@ -125,63 +119,65 @@ var DayDetailPopup = forwardRef(function (_ref2, ref) {
       min = _temp$min === void 0 ? '' : _temp$min,
       _dateTitle = _dt["default"].toDayOfWeek(timestamp) + " " + _dt["default"].toTime(timestamp),
       description = weather[0] && weather[0].description || 'Without description',
-      _pressureTitle = snow > 0.2 ? 'Press.:' : 'Pressure:',
-      _style = isOpen ? STYLE.BLOCK : STYLE.NONE;
+      _isRain = !!rain,
+      _isSnow = snow > 0.02,
+      _pressureTitle = _isRain && _isSnow ? 'Press.:' : 'Pressure:',
+      _style = isOpen ? S_BLOCK : S_NONE;
 
   return /*#__PURE__*/_react["default"].createElement("div", {
-    style: (0, _extends2["default"])({}, _theme.POPUP.CHART, STYLE.ROOT_DIV, _style)
+    style: (0, _extends2["default"])({}, _theme.POPUP.CHART, S_ROOT_DIV, _style)
   }, /*#__PURE__*/_react["default"].createElement(_SvgClose["default"], {
-    style: STYLE.BT_CLOSE,
+    style: S_BT_CLOSE,
     onClose: onClose
   }), /*#__PURE__*/_react["default"].createElement("div", {
-    className: CL.DATE
+    className: CL_DATE
   }, /*#__PURE__*/_react["default"].createElement("span", {
-    style: STYLE.DAY
+    style: S_DAY
   }, _dateTitle)), /*#__PURE__*/_react["default"].createElement("div", null, /*#__PURE__*/_react["default"].createElement("span", {
-    className: CL.DESCR
-  }, description)), /*#__PURE__*/_react["default"].createElement("div", null, /*#__PURE__*/_react["default"].createElement(TitleValue, {
+    className: CL_DESCR
+  }, description)), /*#__PURE__*/_react["default"].createElement("div", null, _isRain && /*#__PURE__*/_react["default"].createElement(TitleValue, {
     title: "Rain:",
-    valueCn: CL.V_RAIN,
+    valueCn: CL_V_RAIN,
     value: rain + "mm"
-  }), snow > 0.02 && /*#__PURE__*/_react["default"].createElement(TitleValue, {
+  }), _isSnow && /*#__PURE__*/_react["default"].createElement(TitleValue, {
     title: "Snow:",
-    valueCn: CL.V_WATER,
+    valueCn: CL_V_WATER,
     value: snow + "mm"
   }), /*#__PURE__*/_react["default"].createElement(TitleValue, {
     title: _pressureTitle,
-    valueCn: CL.V_PRESSURE,
+    valueCn: CL_V_PRESSURE,
     value: pressure + "hPa"
   })), /*#__PURE__*/_react["default"].createElement("div", null, /*#__PURE__*/_react["default"].createElement(TitleValue, {
     title: "Clouds:",
-    valueCn: CL.V_WATER,
+    valueCn: CL_V_WATER,
     value: clouds + "%"
   }), /*#__PURE__*/_react["default"].createElement(TitleValue, {
     title: "Humidity:",
-    valueCn: CL.V_WATER,
+    valueCn: CL_V_WATER,
     value: humidity + "%"
   })), /*#__PURE__*/_react["default"].createElement("div", null, /*#__PURE__*/_react["default"].createElement(TitleValue, {
     title: "Morn:",
-    valueCn: CL.V_DAY,
+    valueCn: CL_V_DAY,
     value: morn
   }), /*#__PURE__*/_react["default"].createElement(TitleValue, {
     title: "Day:",
-    valueCn: CL.V_DAY,
+    valueCn: CL_V_DAY,
     value: day
   }), /*#__PURE__*/_react["default"].createElement(TitleValue, {
     title: "Max:",
-    valueCn: CL.V_DAY,
+    valueCn: CL_V_DAY,
     value: max
   })), /*#__PURE__*/_react["default"].createElement("div", null, /*#__PURE__*/_react["default"].createElement(TitleValue, {
     title: "Eve:",
-    valueCn: CL.V_NIGHT,
+    valueCn: CL_V_NIGHT,
     value: eve
   }), /*#__PURE__*/_react["default"].createElement(TitleValue, {
     title: "Night:",
-    valueCn: CL.V_NIGHT,
+    valueCn: CL_V_NIGHT,
     value: night
   }), /*#__PURE__*/_react["default"].createElement(TitleValue, {
     title: "Min:",
-    valueCn: CL.V_NIGHT,
+    valueCn: CL_V_NIGHT,
     value: min
   })));
 });
--- src/components/left-push-menu/DayDetailPopup.jsx
+++ src/components/left-push-menu/DayDetailPopup.jsx
@@ -6,48 +6,44 @@ import dt from '../../utils/dt';
 import SvgClose from '../zhn-atoms/SvgClose'
 import { POPUP } from '../styles/theme';
 
-const CL = {
-  DATE: 'marker__caption__date',
-  DESCR: 'marker__description',
-  LABEL: 'marker__label',
-  V_RAIN: 'marker__v-rain',
-  V_WATER: 'marker__v-water',
-  V_PRESSURE: 'marker__v-pressure',
-  V_DAY: 'marker__v-day',
-  V_NIGHT: 'marker__v-night'
+const CL_DATE = 'marker__caption__date'
+, CL_DESCR = 'marker__description'
+, CL_LABEL = 'marker__label'
+, CL_V_RAIN = 'marker__v-rain'
+, CL_V_WATER = 'marker__v-water'
+, CL_V_PRESSURE = 'marker__v-pressure'
+, CL_V_DAY = 'marker__v-day'
+, CL_V_NIGHT = 'marker__v-night'
+
+, S_ROOT_DIV = {
+  position: 'absolute',
+  top: 190,
+  left: 200,
+  padding: '8px 8px',
+  lineHeight: 1.5,
+  borderRadius: 4,
+  boxShadow: 'rgba(0, 0, 0, 0.2) 0px 0px 0px 6px',
+  zIndex: 1,
+  transition: 'left 0.5s ease-in 0s'
+}
+, S_BLOCK = { display: 'block' }
+, S_NONE = { display: 'none' }
+, S_BT_CLOSE = {
+  position: 'absolute',
+  top: 7,
+  right: 4
+},
+S_DAY = {
+  borderBottom: '2px solid #8bc34a'
 };
 
-const STYLE = {
-  ROOT_DIV: {
-    position: 'absolute',
-    top: 190,
-    left : 200,
-    padding: '8px 8px',
-    lineHeight: 1.5,
-    borderRadius: 4,
-    boxShadow: 'rgba(0, 0, 0, 0.2) 0px 0px 0px 6px',
-    zIndex: 1,
-    transition: 'left 0.5s ease-in 0s'
-  },
-  BLOCK: {
-    display: 'block'
-  },
-  NONE: {
-    display: 'none'
-  },
-  BT_CLOSE: {
-    position: 'absolute',
-    top: 7,
-    right: 4
-  },
-  DAY: {
-    borderBottom: '2px solid #8bc34a'
-  }
-};
-
-const TitleValue = ({ title, valueCn, value }) => (
+const TitleValue = ({
+  title,
+  valueCn,
+  value
+}) => (
   <>
-   <span className={CL.LABEL}>{title}&nbsp;</span>
+   <span className={CL_LABEL}>{title}&nbsp;</span>
    <span className={valueCn}>
      {value}&nbsp;
    </span>
@@ -68,88 +64,92 @@ const DayDetailPopup = forwardRef(({ onClose }, ref) => {
     })
   }))
 
-  const { dt:timestamp, rain=0, snow=0, clouds=0, humidity='', pressure='', temp={}, weather=[] } = item || {}
+  const { dt:timestamp, rain, snow, clouds=0, humidity='', pressure='', temp={}, weather=[] } = item || {}
   , { morn='', day='', max='', eve='', night='', min='' } = temp
   , _dateTitle = `${dt.toDayOfWeek(timestamp)} ${dt.toTime(timestamp)}`
   , description = (weather[0] && weather[0].description)
        || 'Without description'
-  , _pressureTitle = snow > 0.2 ? 'Press.:' : 'Pressure:'
+  , _isRain = !!rain
+  , _isSnow = snow > 0.02
+  , _pressureTitle = _isRain && _isSnow
+       ? 'Press.:' : 'Pressure:'
   , _style = isOpen
-       ? STYLE.BLOCK
-       : STYLE.NONE;
+       ? S_BLOCK
+       : S_NONE;
 
   return (
     <div style={{
-        ...POPUP.CHART, ...STYLE.ROOT_DIV,
+        ...POPUP.CHART, ...S_ROOT_DIV,
         ..._style
      }}>
       <SvgClose
-        style={STYLE.BT_CLOSE}
+        style={S_BT_CLOSE}
         onClose={onClose}
       />
-      <div className={CL.DATE}>
-        <span style={STYLE.DAY}>
+      <div className={CL_DATE}>
+        <span style={S_DAY}>
           {_dateTitle}
         </span>
       </div>
       <div>
-        <span className={CL.DESCR}>{description}</span>
+        <span className={CL_DESCR}>{description}</span>
       </div>
       <div>
-        <TitleValue title="Rain:"
-          valueCn={CL.V_RAIN}
-          value={`${rain}mm`}
-        />
-        { snow > 0.02 && <TitleValue title="Snow:"
-            valueCn={CL.V_WATER}
+        {_isRain && <TitleValue title="Rain:"
+            valueCn={CL_V_RAIN}
+            value={`${rain}mm`}
+          />
+        }
+        {_isSnow && <TitleValue title="Snow:"
+            valueCn={CL_V_WATER}
             value={`${snow}mm`}
           />
         }
         <TitleValue title={_pressureTitle}
-          valueCn={CL.V_PRESSURE}
+          valueCn={CL_V_PRESSURE}
           value={`${pressure}hPa`}
         />
       </div>
       <div>
         <TitleValue title="Clouds:"
-          valueCn={CL.V_WATER}
+          valueCn={CL_V_WATER}
           value={`${clouds}%`}
         />
         <TitleValue title="Humidity:"
-          valueCn={CL.V_WATER}
+          valueCn={CL_V_WATER}
           value={`${humidity}%`}
         />
       </div>
       <div>
         <TitleValue title="Morn:"
-          valueCn={CL.V_DAY}
+          valueCn={CL_V_DAY}
           value={morn}
         />
         <TitleValue title="Day:"
-          valueCn={CL.V_DAY}
+          valueCn={CL_V_DAY}
           value={day}
         />
         <TitleValue title="Max:"
-          valueCn={CL.V_DAY}
+          valueCn={CL_V_DAY}
           value={max}
         />
       </div>
       <div>
         <TitleValue title="Eve:"
-          valueCn={CL.V_NIGHT}
+          valueCn={CL_V_NIGHT}
           value={eve}
         />
         <TitleValue title="Night:"
-          valueCn={CL.V_NIGHT}
+          valueCn={CL_V_NIGHT}
           value={night}
         />
         <TitleValue title="Min:"
-          valueCn={CL.V_NIGHT}
+          valueCn={CL_V_NIGHT}
           value={min}
         />
       </div>
     </div>
   );
-})
+});
 
 export default DayDetailPopup
