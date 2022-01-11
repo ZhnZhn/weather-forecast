c9542875c1f16617458d249e1c162a97a4e49500
Yevhenii Molodyi <eugmolody@gmail.com>
2022-01-11 14:16:28 +0200

    Add css class name .select-none

--- js/components/views/Caption.js
+++ js/components/views/Caption.js
@@ -9,35 +9,34 @@ var _extends2 = _interopRequireDefault(require("@babel/runtime/helpers/extends")
 
 var _react = _interopRequireDefault(require("../_react"));
 
-//import React from 'react';
-var STYLE = {
-  ROOT_DIV: {
-    display: 'inline',
-    color: '#795548',
-    width: '100%',
-    paddingLeft: 8,
-    marginBottom: 8,
-    borderBottom: '3px solid #795548',
-    fontSize: '24px',
-    fontWeight: 'bold'
-  }
+var CL_SELECT_NONE = 'select-none',
+    ROOT_DIV = {
+  display: 'inline',
+  color: '#795548',
+  width: '100%',
+  paddingLeft: 8,
+  marginBottom: 8,
+  borderBottom: '3px solid #795548',
+  fontSize: '24px',
+  fontWeight: 'bold'
 };
 
 var Caption = function Caption(_ref) {
-  var forecast = _ref.forecast,
-      style = _ref.style;
+  var style = _ref.style,
+      forecast = _ref.forecast;
 
   var _ref2 = forecast || {},
       city = _ref2.city,
       _ref3 = city || {},
       _ref3$name = _ref3.name,
       name = _ref3$name === void 0 ? 'Forecast' : _ref3$name,
-      _ref3$country = _ref3.country,
-      country = _ref3$country === void 0 ? '' : _ref3$country;
+      country = _ref3.country,
+      _caption = [name, country].filter(Boolean).join(':');
 
   return /*#__PURE__*/_react["default"].createElement("div", {
-    style: (0, _extends2["default"])({}, STYLE.ROOT_DIV, style)
-  }, /*#__PURE__*/_react["default"].createElement("span", null, name), /*#__PURE__*/_react["default"].createElement("span", null, ":"), /*#__PURE__*/_react["default"].createElement("span", null, country));
+    className: CL_SELECT_NONE,
+    style: (0, _extends2["default"])({}, ROOT_DIV, style)
+  }, _caption);
 };
 
 var _default = Caption;
--- src/components/views/Caption.jsx
+++ src/components/views/Caption.jsx
@@ -1,29 +1,34 @@
-//import React from 'react';
-import React from '../_react'
+import React from '../_react';
 
-const STYLE = {
-  ROOT_DIV : {
-    display : 'inline',
-    color : '#795548',
-    width : '100%',
-    paddingLeft : 8,
-    marginBottom : 8,
-    borderBottom : '3px solid #795548',
-    fontSize : '24px',
-    fontWeight : 'bold',
-  }
-}
+const CL_SELECT_NONE = 'select-none'
+, ROOT_DIV = {
+  display: 'inline',
+  color: '#795548',
+  width: '100%',
+  paddingLeft: 8,
+  marginBottom: 8,
+  borderBottom: '3px solid #795548',
+  fontSize: '24px',
+  fontWeight: 'bold',
+};
 
-const Caption = ({ forecast, style}) => {
+const Caption = ({
+  style,
+  forecast
+}) => {
   const { city } = forecast || {}
-  , { name='Forecast', country='' } = city || {};
+  , { name='Forecast', country } = city || {}
+  , _caption = [name, country]
+      .filter(Boolean)
+      .join(':');
   return (
-    <div style={{...STYLE.ROOT_DIV, ...style}}>
-      <span>{name}</span>
-      <span>:</span>
-      <span>{country}</span>
+    <div
+      className={CL_SELECT_NONE}
+      style={{...ROOT_DIV, ...style}}
+    >
+      {_caption}
     </div>
-  )
-}
+  );
+};
 
 export default Caption
