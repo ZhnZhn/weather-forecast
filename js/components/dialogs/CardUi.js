bdc7a83bf0fda7be5e5c130e995f4dc60d37afbe
Yevhenii Molodyi <eugmolody@gmail.com>
2022-01-12 17:19:35 +0200

    Replace CardUi by function component

--- js/components/dialogs/CardUi.js
+++ js/components/dialogs/CardUi.js
@@ -5,27 +5,18 @@ var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefau
 exports.__esModule = true;
 exports["default"] = void 0;
 
-var _inheritsLoose2 = _interopRequireDefault(require("@babel/runtime/helpers/inheritsLoose"));
-
 var _react = _interopRequireDefault(require("../_react"));
 
 var _InputSelect = _interopRequireDefault(require("../zhn-m-input/InputSelect"));
 
 var _RaisedButton = _interopRequireDefault(require("../zhn-atoms/RaisedButton"));
 
-var Component = _react["default"].Component;
-var S = {
-  SELECT: {
-    ROOT: {
-      width: '280px'
-    }
+var S_SELECT = {
+  ROOT: {
+    width: 280
   }
-};
-var DF_THEME = {
-  caption: 'Grey',
-  value: 'GREY'
-};
-var _themeOptions = [{
+},
+    _themeOptions = [{
   caption: 'Grey',
   value: 'GREY'
 }, {
@@ -34,44 +25,30 @@ var _themeOptions = [{
 }, {
   caption: 'White',
   value: 'WHITE'
-}];
+}],
+    DF_THEME = _themeOptions[0];
 
-var CardUi =
-/*#__PURE__*/
-function (_Component) {
-  (0, _inheritsLoose2["default"])(CardUi, _Component);
-
-  function CardUi() {
-    return _Component.apply(this, arguments) || this;
-  }
-
-  var _proto = CardUi.prototype;
-
-  _proto.render = function render() {
-    var _this$props = this.props,
-        style = _this$props.style,
-        buttonsStyle = _this$props.buttonsStyle,
-        onSetTheme = _this$props.onSetTheme,
-        onClose = _this$props.onClose;
-    return _react["default"].createElement("div", {
-      style: style
-    }, _react["default"].createElement(_InputSelect["default"], {
-      styleConfig: S.SELECT,
-      caption: "Theme (Default: Grey)",
-      initItem: DF_THEME,
-      options: _themeOptions,
-      onSelect: onSetTheme
-    }), _react["default"].createElement("div", {
-      style: buttonsStyle
-    }, _react["default"].createElement(_RaisedButton["default"], {
-      isPrimary: true,
-      caption: "Close",
-      onClick: onClose
-    })));
-  };
-
-  return CardUi;
-}(Component);
+var CardUi = function CardUi(_ref) {
+  var style = _ref.style,
+      buttonsStyle = _ref.buttonsStyle,
+      onSetTheme = _ref.onSetTheme,
+      onClose = _ref.onClose;
+  return /*#__PURE__*/_react["default"].createElement("div", {
+    style: style
+  }, /*#__PURE__*/_react["default"].createElement(_InputSelect["default"], {
+    styleConfig: S_SELECT,
+    caption: "Theme (Default: Grey)",
+    initItem: DF_THEME,
+    options: _themeOptions,
+    onSelect: onSetTheme
+  }), /*#__PURE__*/_react["default"].createElement("div", {
+    style: buttonsStyle
+  }, /*#__PURE__*/_react["default"].createElement(_RaisedButton["default"], {
+    isPrimary: true,
+    caption: "Close",
+    onClick: onClose
+  })));
+};
 
 var _default = CardUi;
 exports["default"] = _default;
--- src/components/dialogs/CardUi.jsx
+++ src/components/dialogs/CardUi.jsx
@@ -3,48 +3,40 @@ import React from '../_react'
 import InputSelect from '../zhn-m-input/InputSelect'
 import RaisedButton from '../zhn-atoms/RaisedButton'
 
-const { Component } = React
-
-const S = {
-  SELECT: {
-    ROOT: {
-      width: '280px'
-    }
-  }
-};
-
-const DF_THEME = { caption: 'Grey', value: 'GREY' };
-const _themeOptions = [
+const S_SELECT = {
+  ROOT: { width: 280 }
+}
+, _themeOptions = [
   { caption: 'Grey', value: 'GREY' },
   { caption: 'Sand', value: 'SAND' },
   { caption: 'White', value: 'WHITE' }
-];
+]
+, DF_THEME = _themeOptions[0];
 
-class CardUi extends Component {
-  render(){
-    const {
-             style, buttonsStyle,
-             onSetTheme, onClose
-           } = this.props
-    return (
-      <div style={style}>
-        <InputSelect
-          styleConfig={S.SELECT}
-          caption="Theme (Default: Grey)"
-          initItem={DF_THEME}
-          options={_themeOptions}
-          onSelect={onSetTheme}
-        />
-        <div style={buttonsStyle}>
-          <RaisedButton
-            isPrimary={true}
-            caption="Close"
-            onClick={onClose}
-          />
-        </div>
-      </div>
-    );
-  }
-}
+const CardUi = ({
+  style,
+  buttonsStyle,
+  onSetTheme,
+  onClose
+}) => (
+   <div style={style}>
+     <InputSelect
+       styleConfig={S_SELECT}
+       caption="Theme (Default: Grey)"
+       initItem={DF_THEME}
+       options={_themeOptions}
+       onSelect={onSetTheme}
+     />
+     <div style={buttonsStyle}>
+       <RaisedButton
+         isPrimary={true}
+         caption="Close"
+         onClick={onClose}
+       />
+     </div>
+   </div>
+  );
+
+
 
 export default CardUi
