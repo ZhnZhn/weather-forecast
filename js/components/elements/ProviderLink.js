a06bdbd7caeeb754dccded0eb04a6c60966c1ec1
Yevhenii Molodyi <eugmolody@gmail.com>
2022-01-19 11:34:07 +0200

    Edit ProviderLink

--- js/components/elements/ProviderLink.js
+++ js/components/elements/ProviderLink.js
@@ -1,17 +1,13 @@
 "use strict";
 
-var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
-
 exports.__esModule = true;
 exports["default"] = void 0;
 
-var _react = _interopRequireDefault(require("../_react"));
+var _jsxRuntime = require("react/jsx-runtime");
 
-var CL = "data-provider-link";
-var S = {
-  LINK: {
-    color: '#ff9800'
-  }
+var CL = "data-provider-link",
+    S_LINK = {
+  color: '#ff9800'
 };
 
 var ProviderLink = function ProviderLink(_ref) {
@@ -20,16 +16,19 @@ var ProviderLink = function ProviderLink(_ref) {
       prefixCL = _ref.prefixCL,
       _ref$prefix = _ref.prefix,
       prefix = _ref$prefix === void 0 ? 'Powered By ' : _ref$prefix;
-  return _react["default"].createElement("div", {
+  return /*#__PURE__*/(0, _jsxRuntime.jsxs)("div", {
     className: className,
-    style: style
-  }, _react["default"].createElement("span", {
-    className: prefixCL
-  }, prefix), _react["default"].createElement("a", {
-    className: CL,
-    style: S.LINK,
-    href: "https://openweathermap.org/"
-  }, "OpenWeatherMap"));
+    style: style,
+    children: [/*#__PURE__*/(0, _jsxRuntime.jsx)("span", {
+      className: prefixCL,
+      children: prefix
+    }), /*#__PURE__*/(0, _jsxRuntime.jsx)("a", {
+      className: CL,
+      style: S_LINK,
+      href: "https://openweathermap.org/",
+      children: "OpenWeatherMap"
+    })]
+  });
 };
 
 var _default = ProviderLink;
--- src/components/elements/ProviderLink.jsx
+++ src/components/elements/ProviderLink.jsx
@@ -1,30 +1,25 @@
-import React from '../_react'
 
-const CL = "data-provider-link";
-
-const S = {
-  LINK: {
-    color: '#ff9800'
-  }
-};
+const CL = "data-provider-link"
+, S_LINK = { color: '#ff9800' };
 
 const ProviderLink = ({
   className,
   style,
   prefixCL,
   prefix='Powered By '
-}) =>
-<div className={className} style={style}>
-  <span className={prefixCL}>
-    {prefix}
-  </span>
-  <a
-     className={CL}
-     style={S.LINK}
-     href="https://openweathermap.org/"
-  >
-     OpenWeatherMap
-  </a>
-</div>
+}) => (
+  <div className={className} style={style}>
+    <span className={prefixCL}>
+      {prefix}
+    </span>
+    <a
+       className={CL}
+       style={S_LINK}
+       href="https://openweathermap.org/"
+    >
+       OpenWeatherMap
+    </a>
+  </div>
+);
 
 export default ProviderLink
