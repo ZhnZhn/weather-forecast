a3dc0231849e08d0071f9193f2085ce185ba7304
Yevhenii Molodyi <eugmolody@gmail.com>
2023-02-21 19:37:26 +0200

    Edit index.jsx

--- js/index.js
+++ js/index.js
@@ -8,6 +8,14 @@ var _WeatherSaga = _interopRequireDefault(require("./components/WeatherSaga"));
 var _jsxRuntime = require("react/jsx-runtime");
 var _ref = _client["default"] || window.ReactDOM,
   createRoot = _ref.createRoot;
+var NON_STANDARD_DEPREACTED_EVENT_GETTER = 'path';
+if (!Event.prototype.hasOwnProperty(NON_STANDARD_DEPREACTED_EVENT_GETTER)) {
+  Object.defineProperty(Event.prototype, NON_STANDARD_DEPREACTED_EVENT_GETTER, {
+    get: function get() {
+      return this.composedPath();
+    }
+  });
+}
 createRoot(document.getElementById('app')).render( /*#__PURE__*/(0, _jsxRuntime.jsx)(_reactRedux.Provider, {
   store: _store["default"],
   children: /*#__PURE__*/(0, _jsxRuntime.jsx)(_WeatherSaga["default"], {})
--- src/index.jsx
+++ src/index.jsx
@@ -8,6 +8,13 @@ const {
   createRoot
 } = _ReactDOM || window.ReactDOM;
 
+const NON_STANDARD_DEPREACTED_EVENT_GETTER = 'path';
+if (!Event.prototype.hasOwnProperty(NON_STANDARD_DEPREACTED_EVENT_GETTER)) {
+  Object.defineProperty(Event.prototype, NON_STANDARD_DEPREACTED_EVENT_GETTER, {
+    get() { return this.composedPath(); }
+  });
+}
+
 createRoot(document.getElementById('app'))
  .render((
    <Provider store={store}>
