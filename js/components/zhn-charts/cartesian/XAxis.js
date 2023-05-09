86037db645d72db25df41f0258534735e74af163
Yevhenii Molodyi <eugmolody@gmail.com>
2023-05-09 16:17:58 +0300

    Add cartesian XAxis function component

---
+++ js/components/zhn-charts/cartesian/XAxis.js
@@ -0,0 +1,29 @@
+"use strict";
+
+exports.__esModule = true;
+exports.XAxis = void 0;
+var XAxis = function XAxis() {
+  return null;
+};
+exports.XAxis = XAxis;
+XAxis.displayName = 'XAxis';
+XAxis.defaultProps = {
+  allowDecimals: true,
+  hide: false,
+  orientation: 'bottom',
+  width: 0,
+  height: 30,
+  mirror: false,
+  xAxisId: 0,
+  tickCount: 5,
+  type: 'category',
+  padding: {
+    left: 0,
+    right: 0
+  },
+  allowDataOverflow: false,
+  scale: 'auto',
+  reversed: false,
+  allowDuplicatedCategory: true
+};
+//# sourceMappingURL=XAxis.js.map
