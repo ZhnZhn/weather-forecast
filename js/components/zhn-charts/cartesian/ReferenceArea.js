4c5303401c0e0b0b92ba113612a8b08b2de7bcdc
Yevhenii Molodyi <eugmolody@gmail.com>
2023-05-11 17:59:55 +0300

    Add cartesian ReferenceArea function component

---
+++ js/components/zhn-charts/cartesian/ReferenceArea.js
@@ -0,0 +1,98 @@
+"use strict";
+
+var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
+exports.__esModule = true;
+exports.ReferenceArea = void 0;
+var _extends2 = _interopRequireDefault(require("@babel/runtime/helpers/extends"));
+var _classnames = _interopRequireDefault(require("classnames"));
+var _Layer = require("../container/Layer");
+var _Label = require("../component/Label");
+var _CartesianUtils = require("../util/CartesianUtils");
+var _IfOverflowMatches = require("../util/IfOverflowMatches");
+var _DataUtils = require("../util/DataUtils");
+var _Rectangle = require("../shape/Rectangle");
+var _ReactUtils = require("../util/ReactUtils");
+var _cartesianFn = require("./cartesianFn");
+var _jsxRuntime = require("react/jsx-runtime");
+var CL_REFERENCE_AREA = 'recharts-reference-area',
+  CL_REFERENCE_AREA_RECT = CL_REFERENCE_AREA + "-rect",
+  DISCARD = 'discard';
+var getRect = function getRect(hasX1, hasX2, hasY1, hasY2, props) {
+  var xValue1 = props.x1,
+    xValue2 = props.x2,
+    yValue1 = props.y1,
+    yValue2 = props.y2,
+    xAxis = props.xAxis,
+    yAxis = props.yAxis;
+  if (!xAxis || !yAxis) {
+    return null;
+  }
+  var scales = (0, _CartesianUtils.createLabeledScales)({
+      x: xAxis.scale,
+      y: yAxis.scale
+    }),
+    p1 = {
+      x: hasX1 ? scales.x.apply(xValue1, {
+        position: 'start'
+      }) : scales.x.rangeMin,
+      y: hasY1 ? scales.y.apply(yValue1, {
+        position: 'start'
+      }) : scales.y.rangeMin
+    },
+    p2 = {
+      x: hasX2 ? scales.x.apply(xValue2, {
+        position: 'end'
+      }) : scales.x.rangeMax,
+      y: hasY2 ? scales.y.apply(yValue2, {
+        position: 'end'
+      }) : scales.y.rangeMax
+    };
+  return (0, _IfOverflowMatches.ifOverflowMatches)(props, DISCARD) && (!scales.isInRange(p1) || !scales.isInRange(p2)) ? null : (0, _CartesianUtils.rectWithPoints)(p1, p2);
+};
+var ReferenceArea = function ReferenceArea(props) {
+  var x1 = props.x1,
+    x2 = props.x2,
+    y1 = props.y1,
+    y2 = props.y2,
+    className = props.className,
+    clipPathId = props.clipPathId,
+    hasX1 = (0, _DataUtils.isNumOrStr)(x1),
+    hasX2 = (0, _DataUtils.isNumOrStr)(x2),
+    hasY1 = (0, _DataUtils.isNumOrStr)(y1),
+    hasY2 = (0, _DataUtils.isNumOrStr)(y2),
+    shape = props.shape;
+  if (!hasX1 && !hasX2 && !hasY1 && !hasY2 && !shape) {
+    return null;
+  }
+  var rect = getRect(hasX1, hasX2, hasY1, hasY2, props);
+  if (!rect && !shape) {
+    return null;
+  }
+  var clipPath = (0, _IfOverflowMatches.ifOverflowMatches)(props, 'hidden') ? "url(#" + clipPathId + ")" : void 0;
+  return /*#__PURE__*/(0, _jsxRuntime.jsxs)(_Layer.Layer, {
+    className: (0, _classnames["default"])(CL_REFERENCE_AREA, className),
+    children: [ReferenceArea.renderRect(shape, (0, _extends2["default"])({
+      clipPath: clipPath
+    }, (0, _ReactUtils.filterProps)(props, true), rect)), _Label.Label.renderCallByParent(props, rect)]
+  });
+};
+exports.ReferenceArea = ReferenceArea;
+ReferenceArea.displayName = 'ReferenceArea';
+ReferenceArea.defaultProps = {
+  isFront: false,
+  ifOverflow: DISCARD,
+  xAxisId: 0,
+  yAxisId: 0,
+  r: 10,
+  fill: '#ccc',
+  fillOpacity: 0.5,
+  stroke: 'none',
+  strokeWidth: 1
+};
+var _crRectangleElement = function _crRectangleElement(props) {
+  return /*#__PURE__*/(0, _jsxRuntime.jsx)(_Rectangle.Rectangle, (0, _extends2["default"])({}, props, {
+    className: CL_REFERENCE_AREA_RECT
+  }));
+};
+ReferenceArea.renderRect = (0, _cartesianFn.fCreateElement)(_crRectangleElement);
+//# sourceMappingURL=ReferenceArea.js.map
---
+++ src/components/zhn-charts/cartesian/ReferenceArea.jsx
@@ -0,0 +1,103 @@
+import classNames from 'classnames';
+
+import { Layer } from '../container/Layer';
+import { Label } from '../component/Label';
+import { createLabeledScales, rectWithPoints } from '../util/CartesianUtils';
+import { ifOverflowMatches } from '../util/IfOverflowMatches';
+import { isNumOrStr } from '../util/DataUtils';
+import { Rectangle } from '../shape/Rectangle';
+import { filterProps } from '../util/ReactUtils';
+import { fCreateElement } from './cartesianFn';
+
+const CL_REFERENCE_AREA = 'recharts-reference-area'
+, CL_REFERENCE_AREA_RECT = `${CL_REFERENCE_AREA}-rect`
+, DISCARD = 'discard';
+
+const getRect = (
+  hasX1,
+  hasX2,
+  hasY1,
+  hasY2,
+  props
+) => {
+  const {
+    x1: xValue1,
+    x2: xValue2,
+    y1: yValue1,
+    y2: yValue2,
+    xAxis,
+    yAxis
+  } = props;
+  if (!xAxis || !yAxis) {
+    return null;
+  }
+  const scales = createLabeledScales({
+    x: xAxis.scale,
+    y: yAxis.scale
+  })
+  , p1 = {
+    x: hasX1 ? scales.x.apply(xValue1, { position: 'start' }) : scales.x.rangeMin,
+    y: hasY1 ? scales.y.apply(yValue1, { position: 'start' }) : scales.y.rangeMin
+  }
+  , p2 = {
+    x: hasX2 ? scales.x.apply(xValue2, { position: 'end' }) : scales.x.rangeMax,
+    y: hasY2 ? scales.y.apply(yValue2, { position: 'end' }) : scales.y.rangeMax
+  };
+
+  return ifOverflowMatches(props, DISCARD) && (!scales.isInRange(p1) || !scales.isInRange(p2))
+    ? null
+    : rectWithPoints(p1, p2);
+};
+
+export const ReferenceArea = (
+  props
+) => {
+  const { x1, x2, y1, y2, className, clipPathId } = props
+  , hasX1 = isNumOrStr(x1)
+  , hasX2 = isNumOrStr(x2)
+  , hasY1 = isNumOrStr(y1)
+  , hasY2 = isNumOrStr(y2)
+  , { shape } = props;
+
+  if (!hasX1 && !hasX2 && !hasY1 && !hasY2 && !shape) {
+    return null;
+  }
+
+  const rect = getRect(hasX1, hasX2, hasY1, hasY2, props);
+  if (!rect && !shape) {
+    return null;
+  }
+  const clipPath = ifOverflowMatches(props, 'hidden')
+    ? `url(#${clipPathId})`
+    : void 0;
+  return (
+    <Layer className={classNames(CL_REFERENCE_AREA, className)}>
+      {ReferenceArea.renderRect(shape, { clipPath, ...filterProps(props, true), ...rect })}
+      {Label.renderCallByParent(props, rect)}
+    </Layer>
+  );
+}
+
+ReferenceArea.displayName = 'ReferenceArea'
+ReferenceArea.defaultProps = {
+  isFront: false,
+  ifOverflow: DISCARD,
+  xAxisId: 0,
+  yAxisId: 0,
+  r: 10,
+  fill: '#ccc',
+  fillOpacity: 0.5,
+  stroke: 'none',
+  strokeWidth: 1
+}
+
+const _crRectangleElement = (
+  props
+) => (
+  <Rectangle
+    {...props}
+    className={CL_REFERENCE_AREA_RECT}
+  />
+);
+
+ReferenceArea.renderRect = fCreateElement(_crRectangleElement)
