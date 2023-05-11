b088a08bbe94bac700165344743fa88cb8a6d563
Yevhenii Molodyi <eugmolody@gmail.com>
2023-05-11 13:40:28 +0300

    Add chart renderTooltip function

---
+++ js/components/zhn-charts/chart/renderTooltip.js
@@ -0,0 +1,35 @@
+"use strict";
+
+var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
+exports.__esModule = true;
+exports.renderTooltip = void 0;
+var _extends2 = _interopRequireDefault(require("@babel/runtime/helpers/extends"));
+var _uApi = require("../../uApi");
+var _ReactUtils = require("../util/ReactUtils");
+var _Tooltip = require("../component/Tooltip");
+var renderTooltip = function renderTooltip(chartInst) {
+  var props = chartInst.props,
+    state = chartInst.state,
+    children = props.children,
+    tooltipItem = (0, _ReactUtils.findChildByType)(children, _Tooltip.Tooltip);
+  if (!tooltipItem) {
+    return null;
+  }
+  var isTooltipActive = state.isTooltipActive,
+    activeCoordinate = state.activeCoordinate,
+    activePayload = state.activePayload,
+    activeLabel = state.activeLabel,
+    offset = state.offset;
+  return (0, _uApi.cloneElement)(tooltipItem, {
+    viewBox: (0, _extends2["default"])({}, offset, {
+      x: offset.left,
+      y: offset.top
+    }),
+    active: isTooltipActive,
+    label: activeLabel,
+    payload: isTooltipActive ? activePayload : [],
+    coordinate: activeCoordinate
+  });
+};
+exports.renderTooltip = renderTooltip;
+//# sourceMappingURL=renderTooltip.js.map
---
+++ src/components/zhn-charts/chart/renderTooltip.js
@@ -0,0 +1,34 @@
+import { cloneElement } from '../../uApi';
+
+import { findChildByType } from '../util/ReactUtils';
+import { Tooltip } from '../component/Tooltip';
+
+export const renderTooltip = (
+  chartInst
+) => {
+  const {
+    props,
+    state
+  } = chartInst
+  , { children } = props
+  , tooltipItem = findChildByType(children, Tooltip);
+
+  if (!tooltipItem) {
+    return null;
+  }
+
+  const {
+    isTooltipActive,
+    activeCoordinate,
+    activePayload,
+    activeLabel,
+    offset
+  } = state;
+  return cloneElement(tooltipItem, {
+    viewBox: { ...offset, x: offset.left, y: offset.top },
+    active: isTooltipActive,
+    label: activeLabel,
+    payload: isTooltipActive ? activePayload : [],
+    coordinate: activeCoordinate
+  });
+}
