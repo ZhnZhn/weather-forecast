266b644f997cb041a48860e09a8fd771276fd02a
Yevhenii Molodyi <eugmolody@gmail.com>
2023-05-16 15:55:29 +0300

    Add chart DefaultTooltipContent function component

---
+++ js/components/zhn-charts/component/DefaultTooltipContent.js
@@ -0,0 +1,127 @@
+"use strict";
+
+var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
+exports.__esModule = true;
+exports.DefaultTooltipContent = void 0;
+var _extends2 = _interopRequireDefault(require("@babel/runtime/helpers/extends"));
+var _uiApi = require("../../uiApi");
+var _classnames = _interopRequireDefault(require("classnames"));
+var _sortBy2 = _interopRequireDefault(require("lodash/sortBy"));
+var _FnUtils = require("../util/FnUtils");
+var _DataUtils = require("../util/DataUtils");
+var _jsxRuntime = require("react/jsx-runtime");
+var _this = void 0;
+var CL_TOOLTIP_ITEM = "recharts-tooltip-item",
+  CL_TOOLTIP_ITEM_NAME = CL_TOOLTIP_ITEM + "-name",
+  CL_TOOLTIP_ITEM_SEPARATOR = CL_TOOLTIP_ITEM + "-separator",
+  CL_TOOLTIP_ITEM_VALUE = CL_TOOLTIP_ITEM + "-value",
+  CL_TOOLTIP_ITEM_UNIT = CL_TOOLTIP_ITEM + "-unit",
+  CL_TOOLTIP_ITEM_LIST = CL_TOOLTIP_ITEM + "-list",
+  CL_DEFAULT_TOOLTIP = "recharts-default-tooltip",
+  CL_TOOLTIP_LABEL = "recharts-tooltip-label";
+var _defaultFormatter = function _defaultFormatter(value) {
+  return (0, _FnUtils._isArr)(value) && (0, _DataUtils.isNumOrStr)(value[0]) && (0, _DataUtils.isNumOrStr)(value[1]) ? value.join(' ~ ') : value;
+};
+var _renderContent = function _renderContent(props) {
+  var payload = props.payload,
+    separator = props.separator,
+    formatter = props.formatter,
+    itemStyle = props.itemStyle,
+    itemSorter = props.itemSorter;
+  if (payload && payload.length) {
+    var listStyle = {
+        padding: 0,
+        margin: 0
+      },
+      items = (itemSorter ? (0, _sortBy2["default"])(payload, itemSorter) : payload).map(function (entry, i) {
+        if (entry.type === 'none') {
+          return null;
+        }
+        var finalItemStyle = (0, _extends2["default"])({
+          display: 'block',
+          paddingTop: 4,
+          paddingBottom: 4,
+          color: entry.color || '#000'
+        }, itemStyle);
+        var finalFormatter = entry.formatter || formatter || _defaultFormatter;
+        var value = entry.value,
+          name = entry.name;
+        if (finalFormatter && value != null && name != null) {
+          var formatted = finalFormatter(value, name, entry, i, payload);
+          if ((0, _FnUtils._isArr)(formatted)) {
+            value = formatted[0];
+            name = formatted[1];
+          } else {
+            value = formatted;
+          }
+        }
+        return /*#__PURE__*/(0, _jsxRuntime.jsxs)("li", {
+          className: CL_TOOLTIP_ITEM,
+          style: finalItemStyle,
+          children: [(0, _DataUtils.isNumOrStr)(name) ? /*#__PURE__*/(0, _jsxRuntime.jsx)("span", {
+            className: CL_TOOLTIP_ITEM_NAME,
+            children: name
+          }) : null, (0, _DataUtils.isNumOrStr)(name) ? /*#__PURE__*/(0, _jsxRuntime.jsx)("span", {
+            className: CL_TOOLTIP_ITEM_SEPARATOR,
+            children: separator
+          }) : null, /*#__PURE__*/(0, _jsxRuntime.jsx)("span", {
+            className: CL_TOOLTIP_ITEM_VALUE,
+            children: value
+          }), /*#__PURE__*/(0, _jsxRuntime.jsx)("span", {
+            className: CL_TOOLTIP_ITEM_UNIT,
+            children: entry.unit || ''
+          })]
+        }, "tooltip-item-" + i);
+      });
+    return /*#__PURE__*/(0, _jsxRuntime.jsx)("ul", {
+      className: CL_TOOLTIP_ITEM_LIST,
+      style: listStyle,
+      children: items
+    });
+  }
+  return null;
+};
+var DefaultTooltipContent = (0, _uiApi.memo)(function (props) {
+  var wrapperClassName = props.wrapperClassName,
+    contentStyle = props.contentStyle,
+    labelClassName = props.labelClassName,
+    labelStyle = props.labelStyle,
+    label = props.label,
+    labelFormatter = props.labelFormatter,
+    payload = props.payload,
+    finalStyle = (0, _extends2["default"])({
+      margin: 0,
+      padding: 10,
+      backgroundColor: '#fff',
+      border: '1px solid #ccc',
+      whiteSpace: 'nowrap'
+    }, contentStyle),
+    finalLabelStyle = (0, _extends2["default"])({
+      margin: 0
+    }, labelStyle),
+    hasLabel = !(0, _FnUtils._isNil)(label);
+  var finalLabel = hasLabel ? label : '';
+  var wrapperCN = (0, _classnames["default"])(CL_DEFAULT_TOOLTIP, wrapperClassName),
+    labelCN = (0, _classnames["default"])(CL_TOOLTIP_LABEL, labelClassName);
+  if (hasLabel && labelFormatter && payload !== void 0 && payload !== null) {
+    finalLabel = labelFormatter(label, payload);
+  }
+  return /*#__PURE__*/(0, _jsxRuntime.jsxs)("div", {
+    className: wrapperCN,
+    style: finalStyle,
+    children: [/*#__PURE__*/(0, _jsxRuntime.jsx)("p", {
+      className: labelCN,
+      style: finalLabelStyle,
+      children: (0, _uiApi.isValidElement)(finalLabel) ? finalLabel : "" + finalLabel
+    }), _renderContent(_this.props)]
+  });
+});
+exports.DefaultTooltipContent = DefaultTooltipContent;
+DefaultTooltipContent.displayName = 'DefaultTooltipContent';
+DefaultTooltipContent.defaultProps = {
+  separator: ' : ',
+  contentStyle: {},
+  itemStyle: {},
+  labelStyle: {}
+};
+//# sourceMappingURL=DefaultTooltipContent.js.map
---
+++ src/components/zhn-charts/component/DefaultTooltipContent.jsx
@@ -0,0 +1,167 @@
+import {
+  memo,
+  isValidElement
+} from '../../uiApi';
+
+import classNames from 'classnames';
+
+import _sortBy from 'lodash/sortBy';
+import {
+  _isArr,
+  _isNil
+} from '../util/FnUtils';
+
+import { isNumOrStr } from '../util/DataUtils';
+
+const CL_TOOLTIP_ITEM = "recharts-tooltip-item"
+, CL_TOOLTIP_ITEM_NAME = `${CL_TOOLTIP_ITEM}-name`
+, CL_TOOLTIP_ITEM_SEPARATOR = `${CL_TOOLTIP_ITEM}-separator`
+, CL_TOOLTIP_ITEM_VALUE = `${CL_TOOLTIP_ITEM}-value`
+, CL_TOOLTIP_ITEM_UNIT = `${CL_TOOLTIP_ITEM}-unit`
+, CL_TOOLTIP_ITEM_LIST = `${CL_TOOLTIP_ITEM}-list`
+, CL_DEFAULT_TOOLTIP = "recharts-default-tooltip"
+, CL_TOOLTIP_LABEL = "recharts-tooltip-label";
+
+const _defaultFormatter = (
+  value
+) => _isArr(value)
+  && isNumOrStr(value[0])
+  && isNumOrStr(value[1])
+  ? value.join(' ~ ')
+  : value;
+
+const _renderContent = (
+  props
+) => {
+  const {
+    payload,
+    separator,
+    formatter,
+    itemStyle,
+    itemSorter
+  } = props;
+  if (payload && payload.length) {
+    const listStyle = {
+      padding: 0,
+      margin: 0
+    }
+    , items = (itemSorter
+      ? _sortBy(payload, itemSorter)
+      : payload
+    ).map((entry, i) => {
+        if (entry.type === 'none') {
+          return null;
+        }
+        const finalItemStyle = {
+          display: 'block',
+          paddingTop: 4,
+          paddingBottom: 4,
+          color: entry.color || '#000',
+          ...itemStyle
+        };
+        const finalFormatter = entry.formatter
+          || formatter
+          || _defaultFormatter;
+        let {
+          value,
+          name
+        } = entry;
+        if (finalFormatter && value != null && name != null) {
+          const formatted = finalFormatter(
+            value,
+            name,
+            entry,
+            i,
+            payload
+          );
+          if (_isArr(formatted)) {
+            [value, name] = formatted;
+          } else {
+            value = formatted;
+          }
+        }
+        return (
+          <li
+            className={CL_TOOLTIP_ITEM}
+            key={`tooltip-item-${i}`}
+            style={finalItemStyle}
+           >
+            {isNumOrStr(name) ? <span className={CL_TOOLTIP_ITEM_NAME}>{name}</span> : null}
+            {isNumOrStr(name) ? <span className={CL_TOOLTIP_ITEM_SEPARATOR}>{separator}</span> : null}
+            <span className={CL_TOOLTIP_ITEM_VALUE}>
+              {value}
+            </span>
+            <span className={CL_TOOLTIP_ITEM_UNIT}>
+              {entry.unit || ''}
+            </span>
+         </li>
+        );
+    });
+
+    return (
+      <ul
+        className={CL_TOOLTIP_ITEM_LIST}
+        style={listStyle}
+      >
+        {items}
+      </ul>
+    );
+  }
+  return null;
+}
+
+export const DefaultTooltipContent = memo((props) => {
+  const {
+    wrapperClassName,
+    contentStyle,
+    labelClassName,
+    labelStyle,
+    label,
+    labelFormatter,
+    payload
+  } = props
+  , finalStyle = {
+      margin: 0,
+      padding: 10,
+      backgroundColor: '#fff',
+      border: '1px solid #ccc',
+      whiteSpace: 'nowrap',
+      ...contentStyle,
+  }
+  , finalLabelStyle = {
+      margin: 0,
+      ...labelStyle,
+  }
+  , hasLabel = !_isNil(label);
+  let finalLabel = hasLabel ? label : '';
+  const wrapperCN = classNames(CL_DEFAULT_TOOLTIP, wrapperClassName)
+  , labelCN = classNames(CL_TOOLTIP_LABEL, labelClassName);
+
+  if (hasLabel
+    && labelFormatter
+    && payload !== void 0
+    && payload !== null
+  ) {
+    finalLabel = labelFormatter(label, payload);
+  }
+
+  return (
+    <div className={wrapperCN} style={finalStyle}>
+      <p className={labelCN} style={finalLabelStyle}>
+        {isValidElement(finalLabel)
+           ? finalLabel
+           : `${finalLabel}`
+        }
+      </p>
+   {_renderContent(this.props)}
+   </div>
+  );
+})
+
+DefaultTooltipContent.displayName = 'DefaultTooltipContent';
+DefaultTooltipContent.defaultProps = {
+  separator: ' : ',
+  contentStyle: {},
+  itemStyle: {},
+  labelStyle: {}
+};
