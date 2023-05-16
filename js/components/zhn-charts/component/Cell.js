1f8806b0e43246950032441a2e883442709b89f8
Yevhenii Molodyi <eugmolody@gmail.com>
2023-05-16 15:52:29 +0300

    Add chart Cell function component

---
+++ js/components/zhn-charts/component/Cell.js
@@ -0,0 +1,10 @@
+"use strict";
+
+exports.__esModule = true;
+exports.Cell = void 0;
+var Cell = function Cell() {
+  return null;
+};
+exports.Cell = Cell;
+Cell.displayName = 'Cell';
+//# sourceMappingURL=Cell.js.map
---
+++ src/components/zhn-charts/component/Cell.jsx
@@ -0,0 +1,2 @@
+export const Cell = () => null;
+Cell.displayName = 'Cell';
