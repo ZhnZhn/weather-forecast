6fb4da156145165d77cbc7eb73a40c3f39e38b26
Yevhenii Molodyi <eugmolody@gmail.com>
2023-05-17 13:46:03 +0300

    Add chart DataUtils

---
+++ js/components/zhn-charts/util/DataUtils.js
@@ -0,0 +1,111 @@
+"use strict";
+
+var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
+exports.__esModule = true;
+exports.uniqueId = exports.mathSign = exports.isPercent = exports.isNumber = exports.isNumOrStr = exports.interpolateNumber = exports.hasDuplicate = exports.getPercentValue = exports.getAnyElementOfObject = exports.findEntryInArray = void 0;
+var _isNumber2 = _interopRequireDefault(require("lodash/isNumber"));
+var _get2 = _interopRequireDefault(require("lodash/get"));
+var _FnUtils = require("./FnUtils");
+var _getObjectKeys = Object.keys;
+var mathSign = function mathSign(value) {
+  return value === 0 ? 0 : value > 0 ? 1 : -1;
+};
+exports.mathSign = mathSign;
+var isPercent = function isPercent(value) {
+  return (0, _FnUtils._isStr)(value) && value.indexOf('%') === value.length - 1;
+};
+exports.isPercent = isPercent;
+var isNumber = function isNumber(value) {
+  return (0, _isNumber2["default"])(value) && !(0, _FnUtils._isNaN)(value);
+};
+exports.isNumber = isNumber;
+var isNumOrStr = function isNumOrStr(value) {
+  return isNumber(value) || (0, _FnUtils._isStr)(value);
+};
+exports.isNumOrStr = isNumOrStr;
+var idCounter = 0;
+var uniqueId = function uniqueId(prefix) {
+  var id = ++idCounter;
+  return "" + (prefix || '') + id;
+};
+
+/**
+ * Get percent value of a total value
+ * @param {number|string} percent A percent
+ * @param {number} totalValue     Total value
+ * @param {number} defaultValue   The value returned when percent is undefined or invalid
+ * @param {boolean} validate      If set to be true, the result will be validated
+ * @return {number} value
+ */
+exports.uniqueId = uniqueId;
+var getPercentValue = function getPercentValue(percent, totalValue, defaultValue, validate) {
+  if (defaultValue === void 0) {
+    defaultValue = 0;
+  }
+  if (validate === void 0) {
+    validate = false;
+  }
+  if (!isNumber(percent) && !(0, _FnUtils._isStr)(percent)) {
+    return defaultValue;
+  }
+  var value;
+  if (isPercent(percent)) {
+    var index = percent.indexOf('%');
+    value = totalValue * parseFloat(percent.slice(0, index)) / 100;
+  } else {
+    value = +percent;
+  }
+  if ((0, _FnUtils._isNaN)(value)) {
+    value = defaultValue;
+  }
+  if (validate && value > totalValue) {
+    value = totalValue;
+  }
+  return value;
+};
+exports.getPercentValue = getPercentValue;
+var getAnyElementOfObject = function getAnyElementOfObject(obj) {
+  if (!obj) {
+    return null;
+  }
+  var keys = _getObjectKeys(obj);
+  if (keys && keys.length) {
+    return obj[keys[0]];
+  }
+  return null;
+};
+exports.getAnyElementOfObject = getAnyElementOfObject;
+var hasDuplicate = function hasDuplicate(ary) {
+  if (!(0, _FnUtils._isArr)(ary)) {
+    return false;
+  }
+  var len = ary.length,
+    cache = {};
+  for (var i = 0; i < len; i++) {
+    if (!cache[ary[i]]) {
+      cache[ary[i]] = true;
+    } else {
+      return true;
+    }
+  }
+  return false;
+};
+exports.hasDuplicate = hasDuplicate;
+var interpolateNumber = function interpolateNumber(numberA, numberB) {
+  return isNumber(numberA) && isNumber(numberB) ? function (t) {
+    return numberA + t * (numberB - numberA);
+  } : function () {
+    return numberB;
+  };
+};
+exports.interpolateNumber = interpolateNumber;
+var findEntryInArray = function findEntryInArray(arr, specifiedKey, specifiedValue) {
+  if (!(0, _FnUtils._isArr)(arr) || !arr.length) {
+    return null;
+  }
+  return arr.find(function (entry) {
+    return entry && ((0, _FnUtils._isFn)(specifiedKey) ? specifiedKey(entry) : (0, _get2["default"])(entry, specifiedKey)) === specifiedValue;
+  });
+};
+exports.findEntryInArray = findEntryInArray;
+//# sourceMappingURL=DataUtils.js.map
---
+++ src/components/zhn-charts/util/DataUtils.js
@@ -0,0 +1,136 @@
+import _isNumber from 'lodash/isNumber';
+import _get from 'lodash/get';
+
+import {
+  _isNaN,
+  _isArr,
+  _isStr,
+  _isFn
+} from './FnUtils';
+
+const _getObjectKeys = Object.keys;
+
+export const mathSign = (
+  value
+) => value === 0
+  ? 0
+  : value > 0
+     ? 1
+     : -1
+
+
+export const isPercent = (
+  value
+) => _isStr(value)
+  && value.indexOf('%') === value.length - 1;
+
+export const isNumber = (
+  value
+) => _isNumber(value) && !_isNaN(value);
+
+export const isNumOrStr = (
+  value
+) => isNumber(value) || _isStr(value);
+
+let idCounter = 0;
+export const uniqueId = (
+  prefix
+) => {
+  const id = ++idCounter;
+  return `${prefix || ''}${id}`;
+};
+
+/**
+ * Get percent value of a total value
+ * @param {number|string} percent A percent
+ * @param {number} totalValue     Total value
+ * @param {number} defaultValue   The value returned when percent is undefined or invalid
+ * @param {boolean} validate      If set to be true, the result will be validated
+ * @return {number} value
+ */
+export const getPercentValue = (
+  percent,
+  totalValue,
+  defaultValue = 0,
+  validate = false
+) => {
+  if (!isNumber(percent) && !_isStr(percent)) {
+    return defaultValue;
+  }
+
+  let value;
+
+  if (isPercent(percent)) {
+    const index = percent.indexOf('%');
+    value = (totalValue * parseFloat((percent).slice(0, index))) / 100;
+  } else {
+    value = +percent;
+  }
+
+  if (_isNaN(value)) {
+    value = defaultValue;
+  }
+
+  if (validate && value > totalValue) {
+    value = totalValue;
+  }
+
+  return value;
+};
+
+export const getAnyElementOfObject = (obj) => {
+  if (!obj) {
+    return null;
+  }
+
+  const keys = _getObjectKeys(obj);
+  if (keys && keys.length) {
+    return obj[keys[0]];
+  }
+
+  return null;
+};
+
+export const hasDuplicate = (ary) => {
+  if (!_isArr(ary)) {
+    return false;
+  }
+
+  const len = ary.length
+  , cache = {};
+
+  for (let i = 0; i < len; i++) {
+    if (!cache[ary[i]]) {
+      cache[ary[i]] = true;
+    } else {
+      return true;
+    }
+  }
+
+  return false;
+};
+
+export const interpolateNumber = (
+  numberA,
+  numberB
+) => isNumber(numberA) && isNumber(numberB)
+  ? (t) => numberA + t * (numberB - numberA)
+  : () => numberB;
+
+export const findEntryInArray = (
+  arr,
+  specifiedKey,
+  specifiedValue
+) => {
+  if (!_isArr(arr) || !arr.length) {
+    return null;
+  }
+
+  return arr.find(
+    entry => entry
+     && (_isFn(specifiedKey)
+           ? specifiedKey(entry)
+           : _get(entry, specifiedKey)
+         ) === specifiedValue
+  );
+}
