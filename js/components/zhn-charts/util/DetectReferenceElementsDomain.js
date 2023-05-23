fe1da5f970d7e6dea814ba9eab2399efbe6b002d
Yevhenii Molodyi <eugmolody@gmail.com>
2023-05-23 11:49:10 +0300

    Remove chart cartesian reference components

--- js/components/zhn-charts/util/DetectReferenceElementsDomain.js
+++ js/components/zhn-charts/util/DetectReferenceElementsDomain.js
@@ -2,41 +2,9 @@
 
 exports.__esModule = true;
 exports.detectReferenceElementsDomain = void 0;
-var _ReferenceDot = require("../cartesian/ReferenceDot");
-var _ReferenceLine = require("../cartesian/ReferenceLine");
-var _ReferenceArea = require("../cartesian/ReferenceArea");
-var _IfOverflowMatches = require("./IfOverflowMatches");
-var _ReactUtils = require("./ReactUtils");
 var _DataUtils = require("./DataUtils");
 var detectReferenceElementsDomain = function detectReferenceElementsDomain(children, domain, axisId, axisType, specifiedTicks) {
-  var lines = (0, _ReactUtils.findAllByType)(children, _ReferenceLine.ReferenceLine),
-    dots = (0, _ReactUtils.findAllByType)(children, _ReferenceDot.ReferenceDot),
-    elements = [].concat(lines, dots),
-    areas = (0, _ReactUtils.findAllByType)(children, _ReferenceArea.ReferenceArea),
-    idKey = axisType + "Id",
-    valueKey = axisType[0];
   var finalDomain = domain;
-  if (elements.length) {
-    finalDomain = elements.reduce(function (result, el) {
-      if (el.props[idKey] === axisId && (0, _IfOverflowMatches.ifOverflowMatches)(el.props, 'extendDomain') && (0, _DataUtils.isNumber)(el.props[valueKey])) {
-        var value = el.props[valueKey];
-        return [Math.min(result[0], value), Math.max(result[1], value)];
-      }
-      return result;
-    }, finalDomain);
-  }
-  if (areas.length) {
-    var key1 = valueKey + "1",
-      key2 = valueKey + "2";
-    finalDomain = areas.reduce(function (result, el) {
-      if (el.props[idKey] === axisId && (0, _IfOverflowMatches.ifOverflowMatches)(el.props, 'extendDomain') && (0, _DataUtils.isNumber)(el.props[key1]) && (0, _DataUtils.isNumber)(el.props[key2])) {
-        var value1 = el.props[key1],
-          value2 = el.props[key2];
-        return [Math.min(result[0], value1, value2), Math.max(result[1], value1, value2)];
-      }
-      return result;
-    }, finalDomain);
-  }
   if (specifiedTicks && specifiedTicks.length) {
     finalDomain = specifiedTicks.reduce(function (result, tick) {
       if ((0, _DataUtils.isNumber)(tick)) {
--- src/components/zhn-charts/util/DetectReferenceElementsDomain.js
+++ src/components/zhn-charts/util/DetectReferenceElementsDomain.js
@@ -1,8 +1,3 @@
-import { ReferenceDot } from '../cartesian/ReferenceDot';
-import { ReferenceLine } from '../cartesian/ReferenceLine';
-import { ReferenceArea } from '../cartesian/ReferenceArea';
-import { ifOverflowMatches } from './IfOverflowMatches';
-import { findAllByType } from './ReactUtils';
 import { isNumber } from './DataUtils';
 
 export const detectReferenceElementsDomain = (
@@ -12,44 +7,7 @@ export const detectReferenceElementsDomain = (
   axisType,
   specifiedTicks
 ) => {
-  const lines = findAllByType(children, ReferenceLine)
-  , dots = findAllByType(children, ReferenceDot)
-  , elements = [...lines, ...dots]
-  , areas = findAllByType(children, ReferenceArea)
-  , idKey = `${axisType}Id`
-  , valueKey = axisType[0];
-
   let finalDomain = domain;
-  if (elements.length) {
-    finalDomain = elements.reduce((result, el) => {
-      if (el.props[idKey] === axisId
-        && ifOverflowMatches(el.props, 'extendDomain')
-        && isNumber(el.props[valueKey])
-      ) {
-        const value = el.props[valueKey];
-        return [Math.min(result[0], value), Math.max(result[1], value)];
-      }
-      return result;
-    }, finalDomain);
-  }
-  if (areas.length) {
-    const key1 = `${valueKey}1`
-    , key2 = `${valueKey}2`;
-    finalDomain = areas.reduce((result, el) => {
-      if (el.props[idKey] === axisId &&
-        ifOverflowMatches(el.props, 'extendDomain') &&
-        isNumber(el.props[key1]) &&
-        isNumber(el.props[key2])) {
-        const value1 = el.props[key1]
-        , value2 = el.props[key2];
-        return [
-          Math.min(result[0], value1, value2),
-          Math.max(result[1], value1, value2)
-        ];
-      }
-      return result;
-    }, finalDomain);
-  }
   if (specifiedTicks && specifiedTicks.length) {
     finalDomain = specifiedTicks.reduce((result, tick) => {
       if (isNumber(tick)) {
@@ -62,4 +20,4 @@ export const detectReferenceElementsDomain = (
     }, finalDomain);
   }
   return finalDomain;
-};
+}
