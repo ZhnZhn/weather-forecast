7e71e17e79b6c81ac0ba2f81231c8a53c3bc1dc2
Yevhenii Molodyi <eugmolody@gmail.com>
2023-05-24 11:40:32 +0300

    Add hotkeys implementation

---
+++ js/components/hotkeys/hotkeys.js
@@ -0,0 +1,11 @@
+"use strict";
+
+exports.__esModule = true;
+exports.HK_SETTINGS = exports.HK_MENU = exports.HK_FORECAST = void 0;
+var HK_MENU = 'M';
+exports.HK_MENU = HK_MENU;
+var HK_FORECAST = 'F';
+exports.HK_FORECAST = HK_FORECAST;
+var HK_SETTINGS = 'S';
+exports.HK_SETTINGS = HK_SETTINGS;
+//# sourceMappingURL=hotkeys.js.map
---
+++ src/components/hotkeys/hotkeys.js
@@ -0,0 +1,3 @@
+export const HK_MENU = 'M'
+export const HK_FORECAST = 'F'
+export const HK_SETTINGS = 'S'
