a28fbf0ee4ea083080b1caf99950d468fdcb1f44
Yevhenii Molodyi <eugmolody@gmail.com>
2023-06-07 12:38:34 +0300

    Add chart ShallowEqual unit tests

---
+++ js/components/zhn-charts/util/__tests__/ShallowEqual.test.js
@@ -0,0 +1,114 @@
+"use strict";
+
+var _ShallowEqual = require("../ShallowEqual");
+var ARR_VALUE = [1],
+  OBJ_VALUE = {
+    foo: 'bar'
+  },
+  TEST_CONFIGS = [{
+    should: 'return true when the same top level keys have the same top level values, when they are different instances',
+    a: {
+      foo: true,
+      bar: false
+    },
+    b: {
+      foo: true,
+      bar: false
+    },
+    result: true
+  }, {
+    should: 'return true when top level values of an Object are non-primitives but are the same instance',
+    a: {
+      value: ARR_VALUE
+    },
+    b: {
+      value: ARR_VALUE
+    },
+    result: true
+  }, {
+    should: 'return true when top level values of an Array are non-primitives but are the same instance',
+    a: [ARR_VALUE],
+    b: [ARR_VALUE],
+    result: true
+  }, {
+    should: 'return true when top level values of an Array are non-primitives but are the same instance',
+    a: {
+      a: [1]
+    },
+    b: {
+      a: [1]
+    },
+    result: false
+  }, {
+    should: 'return false when a has the same keys but different values',
+    a: {
+      k: 1
+    },
+    b: {
+      k: 2
+    },
+    result: false
+  }, {
+    should: 'return false when a has more keys ',
+    a: {
+      k: 1,
+      x: 5
+    },
+    b: {
+      k: 2
+    },
+    result: false
+  }, {
+    should: 'return false when b has more keys than a',
+    a: {
+      k: 1
+    },
+    b: {
+      k: 2,
+      x: 3
+    },
+    result: false
+  }, {
+    should: 'return false when a contains undefined',
+    a: {
+      first: undefined
+    },
+    b: {
+      second: 'green'
+    },
+    result: false
+  }, {
+    should: 'return true with empty objects',
+    a: {},
+    b: {},
+    result: true
+  }, {
+    should: 'return true when passing the same instance',
+    a: OBJ_VALUE,
+    b: OBJ_VALUE,
+    result: true
+  }, {
+    should: 'return false if there are corresponding elements which are not ===',
+    a: [OBJ_VALUE, OBJ_VALUE],
+    b: [OBJ_VALUE, {
+      foo: 'bar'
+    }],
+    result: false
+  }, {
+    should: 'return true if all corresponding elements are ===',
+    a: [OBJ_VALUE, OBJ_VALUE],
+    b: [OBJ_VALUE, OBJ_VALUE],
+    result: true
+  }];
+describe('shallowEqual', function () {
+  TEST_CONFIGS.forEach(function (_ref) {
+    var should = _ref.should,
+      a = _ref.a,
+      b = _ref.b,
+      result = _ref.result;
+    it(should, function () {
+      expect((0, _ShallowEqual.shallowEqual)(a, b)).toBe(result);
+    });
+  });
+});
+//# sourceMappingURL=ShallowEqual.test.js.map
---
+++ src/components/zhn-charts/util/__tests__/ShallowEqual.test.js
@@ -0,0 +1,75 @@
+import { shallowEqual } from '../ShallowEqual';
+
+const ARR_VALUE = [1]
+, OBJ_VALUE = { foo: 'bar' }
+, TEST_CONFIGS = [
+  {
+    should:'return true when the same top level keys have the same top level values, when they are different instances',
+    a: { foo: true, bar: false },
+    b: { foo: true, bar: false },
+    result: true
+  },{
+    should: 'return true when top level values of an Object are non-primitives but are the same instance',
+    a: { value: ARR_VALUE },
+    b: { value: ARR_VALUE },
+    result: true
+  },{
+    should: 'return true when top level values of an Array are non-primitives but are the same instance',
+    a: [ARR_VALUE],
+    b: [ARR_VALUE],
+    result: true
+  },{
+    should: 'return true when top level values of an Array are non-primitives but are the same instance',
+    a: { a: [1] },
+    b: { a: [1] },
+    result: false
+  },{
+    should: 'return false when a has the same keys but different values',
+    a: { k: 1 },
+    b: { k: 2 },
+    result: false
+  },{
+    should: 'return false when a has more keys ',
+    a: { k: 1, x: 5 },
+    b: { k: 2 },
+    result: false
+  },{
+    should: 'return false when b has more keys than a',
+    a: { k: 1 },
+    b: { k: 2, x: 3 },
+    result: false
+  },{
+    should: 'return false when a contains undefined',
+    a: { first: undefined },
+    b: { second: 'green' },
+    result: false
+  },{
+    should: 'return true with empty objects',
+    a: {},
+    b: {},
+    result: true
+  },{
+    should: 'return true when passing the same instance',
+    a: OBJ_VALUE,
+    b: OBJ_VALUE,
+    result: true
+  },{
+    should: 'return false if there are corresponding elements which are not ===',
+    a: [OBJ_VALUE, OBJ_VALUE],
+    b: [OBJ_VALUE, { foo: 'bar' }],
+    result: false
+  },{
+    should: 'return true if all corresponding elements are ===',
+    a: [OBJ_VALUE, OBJ_VALUE],
+    b: [OBJ_VALUE, OBJ_VALUE],
+    result: true
+  }
+];
+
+describe('shallowEqual', () => {
+  TEST_CONFIGS.forEach(({ should, a, b, result }) => {
+    it(should, () => {
+      expect(shallowEqual(a, b)).toBe(result);
+    });
+  });
+});
