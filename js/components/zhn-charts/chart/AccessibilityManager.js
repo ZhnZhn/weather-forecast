0a555e29cfd55c74941f8c75e8193478850b851a
Yevhenii Molodyi <eugmolody@gmail.com>
2023-06-23 13:45:09 +0300

    Add private method _isNoTooltips to chart AccessibilityManager

--- js/components/zhn-charts/chart/AccessibilityManager.js
+++ js/components/zhn-charts/chart/AccessibilityManager.js
@@ -2,48 +2,56 @@
 
 exports.__esModule = true;
 exports.AccessibilityManager = void 0;
-var LAYOUT_HORIZONTAL = 'horizontal';
-var _isNotLayoutHorizontal = function _isNotLayoutHorizontal(layout) {
-  return layout !== LAYOUT_HORIZONTAL;
-};
-var AccessibilityManager = /*#__PURE__*/function () {
-  function AccessibilityManager() {
+const mathMin = Math.min,
+  mathMax = Math.max;
+const LAYOUT_HORIZONTAL = 'horizontal';
+const _isNotLayoutHorizontal = layout => layout !== LAYOUT_HORIZONTAL;
+class AccessibilityManager {
+  constructor() {
     this.activeIndex = 0;
     this.coordinateList = [];
     this.layout = LAYOUT_HORIZONTAL;
   }
-  var _proto = AccessibilityManager.prototype;
-  _proto.setDetails = function setDetails(_ref) {
-    var _ref$coordinateList = _ref.coordinateList,
-      coordinateList = _ref$coordinateList === void 0 ? [] : _ref$coordinateList,
-      _ref$container = _ref.container,
-      container = _ref$container === void 0 ? null : _ref$container,
-      _ref$layout = _ref.layout,
-      layout = _ref$layout === void 0 ? null : _ref$layout,
-      _ref$offset = _ref.offset,
-      offset = _ref$offset === void 0 ? null : _ref$offset,
-      _ref$mouseHandlerCall = _ref.mouseHandlerCallback,
-      mouseHandlerCallback = _ref$mouseHandlerCall === void 0 ? null : _ref$mouseHandlerCall;
+  setDetails(_ref) {
+    let {
+      coordinateList = [],
+      container = null,
+      layout = null,
+      offset = null,
+      mouseHandlerCallback = null
+    } = _ref;
     this.coordinateList = coordinateList != null ? coordinateList : this.coordinateList;
     this.container = container != null ? container : this.container;
     this.layout = layout != null ? layout : this.layout;
     this.offset = offset != null ? offset : this.offset;
     this.mouseHandlerCallback = mouseHandlerCallback != null ? mouseHandlerCallback : this.mouseHandlerCallback;
-    if (this.activeIndex >= this.coordinateList.length) {
-      this.activeIndex = this.coordinateList.length - 1;
-    }
-  };
-  _proto.focus = function focus() {
+
+    // #3627
+    // Keep activeIndex in the bounds between 0 and the last coordinate index
+    this.activeIndex = mathMin(mathMax(this.activeIndex, 0), this.coordinateList.length - 1);
+  }
+  focus() {
     this.spoofMouse();
-  };
-  _proto.keyboardEvent = function keyboardEvent(e) {
+  }
+
+  // #3627
+  _isNoTooltips() {
+    return this.coordinateList.length === 0;
+  }
+  keyboardEvent(e) {
+    // The AccessibilityManager relies on the Tooltip component. When tooltips suddenly stop existing,
+    // it can cause errors. We use this function to check. We don't want arrow keys to be processed
+    // if there are no tooltips, since that will cause unexpected behavior of users.
+    if (this._isNoTooltips()) {
+      return;
+    }
     switch (e.key) {
       case 'ArrowRight':
         {
           if (_isNotLayoutHorizontal(this.layout)) {
             return;
           }
-          this.activeIndex = Math.min(this.activeIndex + 1, this.coordinateList.length - 1);
+          this.activeIndex = mathMin(this.activeIndex + 1, this.coordinateList.length - 1);
           this.spoofMouse();
           break;
         }
@@ -52,7 +60,7 @@ var AccessibilityManager = /*#__PURE__*/function () {
           if (_isNotLayoutHorizontal(this.layout)) {
             return;
           }
-          this.activeIndex = Math.max(this.activeIndex - 1, 0);
+          this.activeIndex = mathMax(this.activeIndex - 1, 0);
           this.spoofMouse();
           break;
         }
@@ -61,23 +69,31 @@ var AccessibilityManager = /*#__PURE__*/function () {
           break;
         }
     }
-  };
-  _proto.spoofMouse = function spoofMouse() {
+  }
+  spoofMouse() {
     if (_isNotLayoutHorizontal(this.layout)) {
       return;
     }
-    var _this$container$getBo = this.container.getBoundingClientRect(),
-      x = _this$container$getBo.x,
-      y = _this$container$getBo.y,
-      coordinate = this.coordinateList[this.activeIndex].coordinate,
+
+    // This can happen when the tooltips suddenly stop existing as children of the component
+    // That update doesn't otherwise fire events, so we have to double check here.
+    if (this._isNoTooltips()) {
+      return;
+    }
+    const {
+        x,
+        y
+      } = this.container.getBoundingClientRect(),
+      {
+        coordinate
+      } = this.coordinateList[this.activeIndex],
       pageX = x + coordinate,
       pageY = y + this.offset.top;
     this.mouseHandlerCallback({
-      pageX: pageX,
-      pageY: pageY
+      pageX,
+      pageY
     });
-  };
-  return AccessibilityManager;
-}();
+  }
+}
 exports.AccessibilityManager = AccessibilityManager;
 //# sourceMappingURL=AccessibilityManager.js.map
--- src/components/zhn-charts/chart/AccessibilityManager.js
+++ src/components/zhn-charts/chart/AccessibilityManager.js
@@ -1,4 +1,7 @@
 
+const mathMin = Math.min
+, mathMax = Math.max;
+
 const LAYOUT_HORIZONTAL = 'horizontal';
 const _isNotLayoutHorizontal = (
   layout
@@ -23,22 +26,37 @@ export class AccessibilityManager {
       this.layout = layout ?? this.layout;
       this.offset = offset ?? this.offset;
       this.mouseHandlerCallback = mouseHandlerCallback ?? this.mouseHandlerCallback;
-      if (this.activeIndex >= this.coordinateList.length) {
-        this.activeIndex = this.coordinateList.length - 1;
-      }
+
+      // #3627
+      // Keep activeIndex in the bounds between 0 and the last coordinate index
+      this.activeIndex = mathMin(mathMax(this.activeIndex, 0), this.coordinateList.length - 1);
+
     }
 
     focus() {
       this.spoofMouse();
     }
 
+    // #3627
+    _isNoTooltips() {
+      return this.coordinateList.length === 0;
+    }
+
     keyboardEvent(e) {
+
+      // The AccessibilityManager relies on the Tooltip component. When tooltips suddenly stop existing,
+      // it can cause errors. We use this function to check. We don't want arrow keys to be processed
+      // if there are no tooltips, since that will cause unexpected behavior of users.
+      if (this._isNoTooltips()) {
+        return;
+      }
+
       switch (e.key) {
         case 'ArrowRight': {
           if (_isNotLayoutHorizontal(this.layout)) {
             return;
           }
-          this.activeIndex = Math.min(this.activeIndex + 1, this.coordinateList.length - 1);
+          this.activeIndex = mathMin(this.activeIndex + 1, this.coordinateList.length - 1);
           this.spoofMouse();
           break;
         }
@@ -46,7 +64,7 @@ export class AccessibilityManager {
           if (_isNotLayoutHorizontal(this.layout)) {
             return;
           }
-          this.activeIndex = Math.max(this.activeIndex - 1, 0);
+          this.activeIndex = mathMax(this.activeIndex - 1, 0);
           this.spoofMouse();
           break;
         }
@@ -54,9 +72,17 @@ export class AccessibilityManager {
           break;
         }
       }
+
+
     }
     spoofMouse() {
       if (_isNotLayoutHorizontal(this.layout)) {
+        return;
+      }
+
+      // This can happen when the tooltips suddenly stop existing as children of the component
+      // That update doesn't otherwise fire events, so we have to double check here.
+      if (this._isNoTooltips()) {
         return;
       }
 
