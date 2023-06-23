69602b450dc0d805e7e6043d89ec7e9dd79c4794
Yevhenii Molodyi <eugmolody@gmail.com>
2023-06-23 12:58:22 +0300

    Add browser targets option to babel config

--- babel.config.js
+++ babel.config.js
@@ -1,11 +1,24 @@
 module.exports = {
-  presets: [
-    ["@babel/preset-env", { loose: true }],
+  presets: [    
+    ['@babel/preset-env', { 
+       loose: true,
+       useBuiltIns: "entry",
+       corejs: "3",      
+       targets: {
+         browsers: [
+            ">0.25%", 
+            /* support arrow-functions */
+            "not ie 11", 
+            "not op_mini all", 
+            "not android 4.4.3-4.4.4", 
+            "not dead"
+         ] 
+       },
+       exclude: []      
+    }],    
     ["@babel/preset-react", { runtime: "automatic" } ]
   ],
   plugins: [
-    ["@babel/plugin-transform-runtime"],    
-    ["@babel/plugin-proposal-decorators", { legacy: true }],
-    ["@babel/plugin-proposal-class-properties", { loose: true }]
+    ["@babel/plugin-transform-runtime"]    
   ]
 };
