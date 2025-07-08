e8ccffeea02de6386765eb962f72c25158626d00
Yevhenii Molodyi <eugmolody@gmail.com>
2025-07-08 16:13:33 +0300

    Add crShowHide named export to components styleFn module

--- js/components/styleFn.js
+++ js/components/styleFn.js
@@ -1,11 +1,16 @@
 "use strict";
 
+var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
 exports.__esModule = true;
-exports.S_NONE = exports.S_BLOCK = void 0;
+exports.crShowHide = exports.S_NONE = exports.S_BLOCK = void 0;
+var _crCn = _interopRequireDefault(require("./zhn-utils/crCn"));
 const S_BLOCK = exports.S_BLOCK = {
   display: 'block'
 };
 const S_NONE = exports.S_NONE = {
   display: 'none'
 };
+const CL_SHOW_POPUP = 'show-popup';
+const crShowHide = (is, className, withoutAnimation, animationClassName) => is ? [(0, _crCn.default)(className, [!withoutAnimation, animationClassName || CL_SHOW_POPUP]), S_BLOCK] : [className, S_NONE];
+exports.crShowHide = crShowHide;
 //# sourceMappingURL=styleFn.js.map
--- src/components/styleFn.js
+++ src/components/styleFn.js
@@ -1,2 +1,23 @@
+import crCn from './zhn-utils/crCn';
+
 export const  S_BLOCK = { display: 'block' }
 export const  S_NONE = { display: 'none' }
+
+const CL_SHOW_POPUP = 'show-popup';
+export const crShowHide = (
+  is,
+  className,
+  withoutAnimation,
+  animationClassName
+) => is
+  ? [
+      crCn(
+        className,
+        [!withoutAnimation, animationClassName || CL_SHOW_POPUP]
+      ),
+      S_BLOCK
+    ]
+  : [
+      className,
+      S_NONE
+    ]
