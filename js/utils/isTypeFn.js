2a98cc6d1d165395854c4ea3061a8a384e29c91f
Yevhenii Molodyi <eugmolody@gmail.com>
2025-09-01 18:07:41 +0300

    Add function isNumOrStr named export to utils isTypeFn module

--- js/utils/isTypeFn.js
+++ js/utils/isTypeFn.js
@@ -1,8 +1,10 @@
 "use strict";
 
 exports.__esModule = true;
-exports.isStr = exports.isNumber = exports.isNaN = exports.isFn = exports.isBool = exports.isArr = void 0;
+exports.isStr = exports.isPositiveNumber = exports.isNumber = exports.isNumOrStr = exports.isNotEmptyArr = exports.isNaN = exports.isFn = exports.isBool = exports.isArr = void 0;
 const isArr = exports.isArr = Array.isArray;
+const isNotEmptyArr = arr => isArr(arr) && arr.length > 0;
+exports.isNotEmptyArr = isNotEmptyArr;
 const _fIsTypeof = strType => v => typeof v == strType;
 const isFn = exports.isFn = _fIsTypeof('function');
 const isStr = exports.isStr = _fIsTypeof('string');
@@ -11,4 +13,8 @@ const _isTypeNumber = _fIsTypeof('number');
 const isNumber = v => _isTypeNumber(v) && v - v == 0;
 exports.isNumber = isNumber;
 const isNaN = exports.isNaN = Number.isNaN;
+const isPositiveNumber = value => isNumber(value) && value >= 0;
+exports.isPositiveNumber = isPositiveNumber;
+const isNumOrStr = value => isNumber(value) || isStr(value);
+exports.isNumOrStr = isNumOrStr;
 //# sourceMappingURL=isTypeFn.js.map
--- src/utils/isTypeFn.js
+++ src/utils/isTypeFn.js
@@ -1,5 +1,8 @@
 
 export const isArr = Array.isArray
+export const isNotEmptyArr = (
+  arr
+) => isArr(arr) && arr.length > 0
 
 const _fIsTypeof = (
   strType
@@ -11,3 +14,11 @@ export const isBool = _fIsTypeof('boolean')
 const _isTypeNumber = _fIsTypeof('number')
 export const isNumber = v => _isTypeNumber(v) && v-v == 0
 export const isNaN = Number.isNaN
+
+export const isPositiveNumber = (
+  value
+) => isNumber(value) && value >= 0
+
+export const isNumOrStr = (
+  value
+) => isNumber(value) || isStr(value)
