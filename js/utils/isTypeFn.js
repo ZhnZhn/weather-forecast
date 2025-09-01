c30834c182c5cd5249e7cf4f29bb3dcce1db9bc1
Yevhenii Molodyi <eugmolody@gmail.com>
2025-09-01 19:14:04 +0300

    Add function isObj named export to utils isTypeFn module

--- js/utils/isTypeFn.js
+++ js/utils/isTypeFn.js
@@ -1,12 +1,14 @@
 "use strict";
 
 exports.__esModule = true;
-exports.isStr = exports.isPositiveNumber = exports.isNumber = exports.isNumOrStr = exports.isNullOrUndef = exports.isNotEmptyArr = exports.isNaN = exports.isFn = exports.isBool = exports.isArr = void 0;
+exports.isStr = exports.isPositiveNumber = exports.isObj = exports.isNumber = exports.isNumOrStr = exports.isNullOrUndef = exports.isNotEmptyArr = exports.isNaN = exports.isFn = exports.isBool = exports.isArr = void 0;
 const isArr = exports.isArr = Array.isArray;
 const isNotEmptyArr = arr => isArr(arr) && arr.length > 0;
 exports.isNotEmptyArr = isNotEmptyArr;
 const isNullOrUndef = v => v == null;
 exports.isNullOrUndef = isNullOrUndef;
+const isObj = v => v !== null && typeof v === 'object';
+exports.isObj = isObj;
 const _fIsTypeof = strType => v => typeof v == strType;
 const isFn = exports.isFn = _fIsTypeof('function');
 const isStr = exports.isStr = _fIsTypeof('string');
--- src/utils/isTypeFn.js
+++ src/utils/isTypeFn.js
@@ -6,6 +6,9 @@ export const isNotEmptyArr = (
 
 export const isNullOrUndef = v => v == null
 
+export const isObj = v => v !== null
+  && typeof v === 'object'
+
 const _fIsTypeof = (
   strType
 ) => v => typeof v == strType
