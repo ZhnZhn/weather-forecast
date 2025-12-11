9cd51af49846e4d011b995359d848d081376dc6e
Yevhenii Molodyi <eugmolody@gmail.com>
2025-12-11 20:08:23 +0200

    Remove not used interval options from charts cartesian getTicks module

--- js/components/zhn-charts/cartesian/getTicks.js
+++ js/components/zhn-charts/cartesian/getTicks.js
@@ -1,36 +1,12 @@
 "use strict";
 
 exports.__esModule = true;
-exports.getEveryNThTick = getEveryNThTick;
-exports.getNumberIntervalTicks = getNumberIntervalTicks;
 exports.getTicks = getTicks;
 var _isTypeFn = require("../../../utils/isTypeFn");
 var _DataUtils = require("../util/DataUtils");
 var _DOMUtils = require("../util/DOMUtils");
 var _Global = require("../util/Global");
 var _getEveryNthWithCondition = require("../util/getEveryNthWithCondition");
-/**
- * Given an array of ticks, find N, the lowest possible number for which every
- * nTH tick in the ticks array isShow == true and return the array of every nTh tick.
- * @param {CartesianTickItem[]} ticks An array of CartesianTickItem with the
- * information whether they can be shown without overlapping with their neighbour isShow.
- * @returns {CartesianTickItem[]} Every nTh tick in an array.
- */
-function getEveryNThTick(ticks) {
-  let N = 1;
-  let previous = (0, _getEveryNthWithCondition.getEveryNthWithCondition)(ticks, N, tickItem => tickItem.isShow);
-  while (N <= ticks.length) {
-    if (previous !== undefined) {
-      return previous;
-    }
-    N++;
-    previous = (0, _getEveryNthWithCondition.getEveryNthWithCondition)(ticks, N, tickItem => tickItem.isShow);
-  }
-  return ticks.slice(0, 1);
-}
-function getNumberIntervalTicks(ticks, interval) {
-  return (0, _getEveryNthWithCondition.getEveryNthWithCondition)(ticks, interval + 1);
-}
 const _crSizeKey = orientation => orientation === 'top' || orientation === 'bottom' ? 'width' : 'height';
 const _crSign = result => result.length >= 2 ? (0, _DataUtils.mathSign)(result[1].coordinate - result[0].coordinate) : 1;
 const _crStartEnd = (_isSizeKeyWidth, x, y, width, height, sign) => {
@@ -75,105 +51,20 @@ function getTicksEnd(_ref) {
       })[sizeKey] + unitSize;
     if (i === len - 1) {
       const gap = sign * (entry.coordinate + sign * size / 2 - end);
-      result[i] = entry = {
-        ...entry,
+      result[i] = entry = Object.assign({}, entry, {
         tickCoord: gap > 0 ? entry.coordinate - gap * sign : entry.coordinate
-      };
+      });
     } else {
-      result[i] = entry = {
-        ...entry,
+      result[i] = entry = Object.assign({}, entry, {
         tickCoord: entry.coordinate
-      };
+      });
     }
     const isShow = sign * (entry.tickCoord - sign * size / 2 - start) >= 0 && sign * (entry.tickCoord + sign * size / 2 - end) <= 0;
     if (isShow) {
       end = entry.tickCoord - sign * (size / 2 + minTickGap);
-      result[i] = {
-        ...entry,
+      result[i] = Object.assign({}, entry, {
         isShow: true
-      };
-    }
-  }
-  return result;
-}
-function getTicksStart(_ref2, preserveEnd) {
-  let {
-    ticks,
-    tickFormatter,
-    viewBox,
-    orientation,
-    minTickGap,
-    unit,
-    fontSize,
-    letterSpacing
-  } = _ref2;
-  const {
-      x,
-      y,
-      width,
-      height
-    } = viewBox,
-    sizeKey = _crSizeKey(orientation),
-    result = (ticks || []).slice()
-    // we need add the width of 'unit' only when sizeKey === 'width'
-    ,
-    _isSizeKeyWidth = sizeKey === 'width',
-    unitSize = unit && _isSizeKeyWidth ? (0, _DOMUtils.getStringSize)(unit, {
-      fontSize,
-      letterSpacing
-    })[sizeKey] : 0,
-    len = result.length,
-    sign = _crSign(result);
-  let [start, end] = _crStartEnd(_isSizeKeyWidth, x, y, width, height, sign);
-  if (preserveEnd) {
-    // Try to guarantee the tail to be displayed
-    let tail = ticks[len - 1];
-    const tailContent = (0, _isTypeFn.isFn)(tickFormatter) ? tickFormatter(tail.value, len - 1) : tail.value,
-      tailSize = (0, _DOMUtils.getStringSize)(tailContent, {
-        fontSize,
-        letterSpacing
-      })[sizeKey] + unitSize,
-      tailGap = sign * (tail.coordinate + sign * tailSize / 2 - end);
-    result[len - 1] = tail = {
-      ...tail,
-      tickCoord: tailGap > 0 ? tail.coordinate - tailGap * sign : tail.coordinate
-    };
-    const isTailShow = sign * (tail.tickCoord - sign * tailSize / 2 - start) >= 0 && sign * (tail.tickCoord + sign * tailSize / 2 - end) <= 0;
-    if (isTailShow) {
-      end = tail.tickCoord - sign * (tailSize / 2 + minTickGap);
-      result[len - 1] = {
-        ...tail,
-        isShow: true
-      };
-    }
-  }
-  const count = preserveEnd ? len - 1 : len;
-  for (let i = 0; i < count; i++) {
-    let entry = result[i];
-    const content = (0, _isTypeFn.isFn)(tickFormatter) ? tickFormatter(entry.value, i) : entry.value,
-      size = (0, _DOMUtils.getStringSize)(content, {
-        fontSize,
-        letterSpacing
-      })[sizeKey] + unitSize;
-    if (i === 0) {
-      const gap = sign * (entry.coordinate - sign * size / 2 - start);
-      result[i] = entry = {
-        ...entry,
-        tickCoord: gap < 0 ? entry.coordinate - gap * sign : entry.coordinate
-      };
-    } else {
-      result[i] = entry = {
-        ...entry,
-        tickCoord: entry.coordinate
-      };
-    }
-    const isShow = sign * (entry.tickCoord - sign * size / 2 - start) >= 0 && sign * (entry.tickCoord + sign * size / 2 - end) <= 0;
-    if (isShow) {
-      start = entry.tickCoord + sign * (size / 2 + minTickGap);
-      result[i] = {
-        ...entry,
-        isShow: true
-      };
+      });
     }
   }
   return result;
@@ -193,7 +84,7 @@ function getTicks(props, fontSize, letterSpacing) {
     return [];
   }
   if ((0, _DataUtils.isNumber)(interval) || _Global.IS_SSR) {
-    return getNumberIntervalTicks(ticks, typeof interval === 'number' && (0, _DataUtils.isNumber)(interval) ? interval : 0);
+    return (0, _getEveryNthWithCondition.getEveryNthWithCondition)(ticks, typeof interval === 'number' && (0, _DataUtils.isNumber)(interval) ? interval + 1 : 1);
   }
   const _tickOptions = {
     ticks,
@@ -205,10 +96,6 @@ function getTicks(props, fontSize, letterSpacing) {
     fontSize,
     letterSpacing
   };
-  if (interval === 'equidistantPreserveStart') {
-    return getEveryNThTick(getTicksStart(_tickOptions));
-  }
-  const candidates = interval === 'preserveStart' || interval === 'preserveStartEnd' ? getTicksStart(_tickOptions, interval === 'preserveStartEnd') : getTicksEnd(_tickOptions);
-  return candidates.filter(entry => entry.isShow);
+  return getTicksEnd(_tickOptions).filter(entry => entry.isShow);
 }
 //# sourceMappingURL=getTicks.js.map
--- src/components/zhn-charts/cartesian/getTicks.js
+++ src/components/zhn-charts/cartesian/getTicks.js
@@ -4,29 +4,6 @@ import { mathSign, isNumber } from '../util/DataUtils';
 import { getStringSize } from '../util/DOMUtils';
 import { IS_SSR } from '../util/Global';
 import { getEveryNthWithCondition } from '../util/getEveryNthWithCondition';
-/**
- * Given an array of ticks, find N, the lowest possible number for which every
- * nTH tick in the ticks array isShow == true and return the array of every nTh tick.
- * @param {CartesianTickItem[]} ticks An array of CartesianTickItem with the
- * information whether they can be shown without overlapping with their neighbour isShow.
- * @returns {CartesianTickItem[]} Every nTh tick in an array.
- */
-export function getEveryNThTick(ticks) {
-  let N = 1;
-  let previous = getEveryNthWithCondition(ticks, N, tickItem => tickItem.isShow);
-  while (N <= ticks.length) {
-    if (previous !== undefined) {
-      return previous;
-    }
-    N++;
-    previous = getEveryNthWithCondition(ticks, N, tickItem => tickItem.isShow);
-  }
-  return ticks.slice(0, 1);
-}
-
-export function getNumberIntervalTicks(ticks, interval) {
-  return getEveryNthWithCondition(ticks, interval + 1);
-}
 
 const _crSizeKey = (
   orientation
@@ -131,95 +108,6 @@ function getTicksEnd({
   return result;
 }
 
-function getTicksStart({
-  ticks,
-  tickFormatter,
-  viewBox,
-  orientation,
-  minTickGap,
-  unit,
-  fontSize,
-  letterSpacing },
-  preserveEnd
-) {
-  const {
-    x,
-    y,
-    width,
-    height
-  } = viewBox
-  , sizeKey = _crSizeKey(orientation)
-  , result = (ticks || []).slice()
-  // we need add the width of 'unit' only when sizeKey === 'width'
-  , _isSizeKeyWidth = sizeKey === 'width'
-  , unitSize = unit && _isSizeKeyWidth
-      ? getStringSize(unit, { fontSize, letterSpacing })[sizeKey]
-      : 0
-  , len = result.length
-  , sign = _crSign(result);
-
-  let [
-    start,
-    end
-  ] = _crStartEnd(
-    _isSizeKeyWidth,
-    x,
-    y,
-    width,
-    height,
-    sign
-  );
-
-  if (preserveEnd) {
-    // Try to guarantee the tail to be displayed
-    let tail = ticks[len - 1];
-    const tailContent = isFn(tickFormatter) ? tickFormatter(tail.value, len - 1) : tail.value
-    , tailSize = getStringSize(tailContent, { fontSize, letterSpacing })[sizeKey] + unitSize
-    , tailGap = sign * (tail.coordinate + (sign * tailSize) / 2 - end);
-    result[len - 1] = tail = {
-      ...tail,
-      tickCoord: tailGap > 0
-        ? tail.coordinate - tailGap * sign
-        : tail.coordinate
-    };
-    const isTailShow = sign * (tail.tickCoord - (sign * tailSize) / 2 - start) >= 0
-      && sign * (tail.tickCoord + (sign * tailSize) / 2 - end) <= 0;
-    if (isTailShow) {
-      end = tail.tickCoord - sign * (tailSize / 2 + minTickGap);
-      result[len - 1] = { ...tail, isShow: true };
-    }
-  }
-  const count = preserveEnd ? len - 1 : len;
-  for (let i = 0; i < count; i++) {
-    let entry = result[i];
-    const content = isFn(tickFormatter)
-      ? tickFormatter(entry.value, i)
-      : entry.value
-    , size = getStringSize(content, { fontSize, letterSpacing })[sizeKey] + unitSize
-    if (i === 0) {
-      const gap = sign * (entry.coordinate - (sign * size) / 2 - start);
-      result[i] = entry = {
-        ...entry,
-        tickCoord: gap < 0
-          ? entry.coordinate - gap * sign
-          : entry.coordinate,
-      };
-    } else {
-      result[i] = entry = {
-        ...entry,
-        tickCoord: entry.coordinate
-      };
-    }
-    const isShow = sign * (entry.tickCoord - (sign * size) / 2 - start) >= 0
-      && sign * (entry.tickCoord + (sign * size) / 2 - end) <= 0;
-    if (isShow) {
-      start = entry.tickCoord + sign * (size / 2 + minTickGap);
-      result[i] = { ...entry, isShow: true };
-    }
-  }
-  return result;
-}
-
 export function getTicks(
   props,
   fontSize,
@@ -241,9 +129,11 @@ export function getTicks(
   }
 
   if (isNumber(interval) || IS_SSR) {
-    return getNumberIntervalTicks(
+    return getEveryNthWithCondition(
       ticks,
-      typeof interval === 'number' && isNumber(interval) ? interval : 0
+      typeof interval === 'number' && isNumber(interval)
+        ? interval + 1
+        : 1
     );
   }
 
@@ -258,14 +148,7 @@ export function getTicks(
     letterSpacing
   };
 
-  if (interval === 'equidistantPreserveStart') {
-    return getEveryNThTick(getTicksStart(_tickOptions));
-  }
-
-  const candidates = interval === 'preserveStart' || interval === 'preserveStartEnd'
-    ? getTicksStart(_tickOptions, interval === 'preserveStartEnd')
-    : getTicksEnd(_tickOptions);
-
-  return candidates
+  
+  return getTicksEnd(_tickOptions)
     .filter(entry => entry.isShow);
 }
