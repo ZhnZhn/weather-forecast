fe8d436d98baf793ca7b44ef1fb7bbddc8ac2ce6
Yevhenii Molodyi <eugmolody@gmail.com>
2026-06-16 15:30:58 +0300

    Add lib chunk splitting configuration

---
+++ webpack.prod.js
@@ -0,0 +1,75 @@
+'use strict'
+
+const path = require('path')
+, webpack = require('webpack')
+, HtmlWebpackPlugin = require('html-webpack-plugin')
+, babelConfig = require('./babel.config')
+, TerserPlugin = require('terser-webpack-plugin');
+
+module.exports = {
+  mode: "production",
+  cache: true,
+  entry: {
+    app: {
+      import: path.resolve('src', 'index.jsx'),
+      dependOn: ['lib','dompurify']
+    },
+    lib: [       
+       "react", "react-dom",
+       "redux", "react-redux", 
+       "redux-saga","redux-saga/effects",                                                             
+    ],
+    dompurify: [
+       "dompurify"       
+    ]
+  },
+  externals: {        
+    "leaflet": {
+       "commonjs": "leaflet",
+       "amd": "leaflet",
+       "root": "L"
+    }    
+  },
+  output: {
+      path: path.resolve('app'),
+      filename: "[name]_[chunkhash].js",
+      chunkFilename: "[name]_[chunkhash].js",
+      publicPath: 'app/'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+             cacheDirectory: true,
+             ...babelConfig
+          }
+        },
+        include: [
+          path.join(__dirname),
+          path.join(__dirname, "src"),
+        ]
+      }
+    ]
+  },
+  resolve: {
+    modules: ['node_modules'],
+    extensions: ['.js', '.jsx']    
+  },
+  plugins : [
+    new HtmlWebpackPlugin({
+      minify: false,
+      filename: path.resolve('index.html'),
+      template: path.resolve('template', 'index.ejs'),
+      inject: false
+    })
+  ],
+  optimization: {
+    runtimeChunk: 'single',
+    minimize: true,
+    minimizer: [new TerserPlugin()]
+  }
+}
