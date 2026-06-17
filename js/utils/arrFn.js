b4dc6556f4c4a3af6ee98441debf0ffc378f4efc
Yevhenii Molodyi <eugmolody@gmail.com>
2026-06-17 14:15:14 +0300

    Add utils arrFn module

---
+++ js/utils/arrFn.js
@@ -0,0 +1,7 @@
+"use strict";
+
+exports.__esModule = true;
+exports.joinByCollon2 = void 0;
+const joinByCollon2 = (v1, v2) => [v1, v2].filter(Boolean).join(': ');
+exports.joinByCollon2 = joinByCollon2;
+//# sourceMappingURL=arrFn.js.map
---
+++ src/utils/arrFn.js
@@ -0,0 +1,7 @@
+
+export const joinByCollon2 = (
+  v1,
+  v2
+) => [v1, v2]
+  .filter(Boolean)
+  .join(': ')
